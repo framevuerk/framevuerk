013e175b254eca04d9ac7f746536e18771bb0c4b
nainemom <nainemom@gmail.com>
2017-05-21 19:07:41 +0430

    re-write some functionality

--- src/components/fvMenu/index.js
+++ src/components/fvMenu/index.js
@@ -10,20 +10,25 @@ export default ({
         sheet: {
             type: Boolean,
             default: false
+        },
+        title: {
+            type: String,
+            default: ''
         }
     },
     data: function(){
         return {
             pShow: false,
-            highlightedOption: null
+            highlightedOption: null,
+            pParam:  null
         }
     },
     methods: {
         pFocus: function(){
             this.$refs.justFocusEl.focus();
-           },
-        open: function(domElem=null){
-            this.pSetPosition(domElem);
+        },
+        open: function(param=null){
+            this.pParam = param;
             this.pShow = true;
             this.$emit('open');
             utility.doIt( ()=>{
@@ -32,53 +37,17 @@ export default ({
         },
         close: function(){
             this.pShow = false;
+            this.pParam = null;
             this.$emit('close');
         },
         toggle: function(domElem=null){
             this[this.pShow?'close':'open'](domElem);
         },
-        pSetPosition: function(domElem = null){
-            let targetOffset;
-            if( this.domElem === null || this.sheet ){
-                targetOffset = {
-                    left: 'initial',
-                    top: 'initial',
-                    width: 'initial',
-                    height: 'initial'
-                }
-            }
-            else{
-                const margin = 15;
-                const windowOffset = utility.windowOffset();
-                const elemOffset = {
-                    left: domElem.pageX,
-                    top: domElem.pageY
-                }
-                targetOffset = {
-                    left: elemOffset.left + margin,
-                    top: elemOffset.top + margin,
-                    width: this.menuSize.width,
-                    height: this.menuSize.height
-                }
-                if( targetOffset.left + targetOffset.width > windowOffset.width ){
-                    targetOffset.left = windowOffset.width - targetOffset.width - margin;
-                }
-                if( targetOffset.top + targetOffset.height > windowOffset.height ){
-                    targetOffset.top = windowOffset.height - targetOffset.height - margin;
-                }
-            }
-            const menu = this.$refs.pMenu;
-            menu.style.top = `${targetOffset.top}px`;
-            menu.style.left = `${targetOffset.left}px`;
-            menu.style.height = `${targetOffset.height}px`;
-            menu.style.width = `${targetOffset.width}px`;
-
-        },
         clickOption: function(option){
             if( !option.disabled ){
-                this.$emit('click', option.key);
-                option.action();
-                //this.close();
+                this.$emit('click', option.key, this.pParam);
+                option.action(this.pParam);
+                this.pFocus();
             }
         },
         highlightOption: function(option={index:null}){
@@ -125,14 +94,6 @@ export default ({
                 });
             });
             return ret;
-        },
-        menuSize(){
-            const width = 200;
-            const itemHeight = 45;
-            return {
-                width,
-                height: itemHeight * this.pOptions.length
-            }
         },
         animationName(){
             return this.sheet? 'fv-sheet': 'fv-menu';
