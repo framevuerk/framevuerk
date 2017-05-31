9156bf40dc99811451b2c380f4293a90401fc0db
nainemom <nainemom@gmail.com>
2017-05-31 20:48:03 +0430

    refactored

--- src/components/fvFilepicker/index.js
+++ src/components/fvFilepicker/index.js
@@ -3,6 +3,9 @@ import template from './template.pug'
 export default ({
     template: template,
     props: {
+        value: {
+            default: null
+        },
         extension: {
             default: 'jpg,png,gif'
         },
@@ -13,35 +16,40 @@ export default ({
         multiple: {
             type: Boolean,
             default: false
+        },
+        type: {
+            type: String,
+            default: 'blob' // or file
         }
     },
     data: function(){
         return {
             invalid: false,
             focus: false,
-            displayValue: 'انتخاب',
+            displayValue: '',
             selected: false
         }
     },
     methods: {
-        checkInvalid: function(){
+        checkInvalid(){
             if( this.required ){
                 const filesCount = this.$refs.fileinput.files.length;
                 this.invalid = filesCount > 0? false: true;
 
             }
         },
-        choose: function(event){
+        choose(event){
             //event.preventDefault();
             this.$refs.fileinput.click();
             //event.preventDefault();
             //event.stopPropagation();
             //alert('clicked');
         },
-        pFocus: function(el="input"){
+        pFocus(el="input"){
             this.$refs.btnEl.$el.focus();
         },
-        pick: function(){
+        pick(){ // here, should fire when input type file changed by user
+            console.log('picked')
             const loadAsBlob = (file) => {
                 return new Promise((resolve, reject) => {
                     const reader = new FileReader();
@@ -60,28 +68,61 @@ export default ({
             let j = 0;
             const filesCount = this.$refs.fileinput.files.length;
             if( filesCount === 0 ){
-                this.displayValue = 'انتخاب';
-                this.selected = false;
                 this.$emit('input', this.multiple? []: null );
             }
             else{
-                for( let i = 0; i < filesCount; i++ ){
-                    loadAsBlob( this.$refs.fileinput.files[i] ).then( (result) => {
-                        value.push( result );
-                        j++;
-                        if( j === filesCount ){
-                            this.displayValue = `${filesCount} فایل انتخاب شد!`;
-                            this.selected = true;
-                            this.$emit('input', this.multiple? value: value[0] );
+                switch( this.type ){
+                    case 'blob':
+                        for( let i = 0; i < filesCount; i++ ){
+                            loadAsBlob( this.$refs.fileinput.files[i] ).then( (result) => {
+                                value.push( result );
+                                j++;
+                                if( j === filesCount ){
+                                    this.$emit('input', this.multiple? value: value[0] );
+                                }
+                            });
                         }
-                    });;
+                        break;
+                    case 'file':
+                        this.$emit('input', this.multiple? this.$refs.fileinput.files: this.$refs.fileinput.files[0] );
+                        break;
                 }
+            }
+        },
+        valueChanges(){
+            if( this.value == null ){
+                this.displayValue = 'انتخاب';
+                this.selected = false;
+                return;
+            }
+            this.selected = true;
+            switch( this.type ){
+                case 'blob':
+                    if( this.multiple ){
+                        this.displayValue = `${filesCount} فایل انتخاب شد!`;
+                    }
+                    else{
+                        this.displayValue = `فایل انتخاب شد!`;
+                    }
+                    break;
+                case 'file':
+                    if( this.multiple ){
+                        this.displayValue = `${filesCount} فایل انتخاب شد!`;
+                    }
+                    else{
+                        this.displayValue = this.value.name;
+                    }
+                    break;
             }
         }
     },
     watch: {
-        value: function(){
+        value(){
             this.checkInvalid();
+            this.valueChanges();
         }
+    },
+    created(){
+        this.valueChanges();
     }
 })
