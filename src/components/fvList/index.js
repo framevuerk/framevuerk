64c6eda565956d6dc4986c2b884e1aea9d6a79ef
nainemom <nainemom@gmail.com>
2017-06-11 15:33:57 +0430

    refactored

--- src/components/fvList/index.js
+++ src/components/fvList/index.js
@@ -11,7 +11,7 @@ export default ({
     methods: {
         clickItem: function(item){
             if( !item.disabled ){
-                this.$emit('click', item.key);
+                this.$emit('click-item', item);
                 item.action();
             }
         }
@@ -19,15 +19,18 @@ export default ({
     computed: {
         pItems: function(){
             let ret = [];
-            this.items.forEach( (value)=>{
-                ret.push({
+            this.items.forEach( (value, index)=>{
+                value = typeof value != 'object' || value === null? {text: value}: value;
+                ret.push(Object.assign(value, {
+                    index: value.index || index,
                     key: value.key || value.text || value,
                     icon: value.icon || false,
                     text: value.text || value,
                     disabled: value.disabled || false,
                     selected: value.selected || false,
+                    highlighted: value.highlighted || false,
                     action: value.action || new Function()
-                });
+                }));
             });
             return ret;
         }
