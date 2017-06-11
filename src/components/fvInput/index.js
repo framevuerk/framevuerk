d8bd06501dea429a0626a65c87b73fdababd073a
nainemom <nainemom@gmail.com>
2017-06-11 15:35:22 +0430

    support disabled

--- src/components/fvInput/index.js
+++ src/components/fvInput/index.js
@@ -15,6 +15,10 @@ export default ({
             type: Boolean,
             default: false
         },
+        disabled: {
+            type: Boolean,
+            default: false
+        },
         placeholder: {
             type: String,
             default : ''
