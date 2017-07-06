9d42f88b022b7511bbbbdb1b1631b32fd4a091b8
nainemom <nainemom@gmail.com>
2017-07-06 09:11:32 +0430

    refactored and add new components

--- src/index.js
+++ src/index.js
@@ -1,58 +1,37 @@
-// Package information
 const pkg = require('../package.json');
-
-// Global Styles
 import './style.scss';
-
-// Components
-import fvMain from './components/fvMain.vue';
-import fvHeader from './components/fvHeader.vue';
-import fvFooter from './components/fvFooter.vue';
-import fvContent from './components/fvContent.vue';
-import fvSidebar from './components/fvSidebar.vue';
-import fvButton from './components/fvButton.vue';
-import fvDialog from './components/fvDialog.vue';
-import fvRadio from './components/fvRadio.vue';
-import fvList from './components/fvList.vue';
-import fvMenu from './components/fvMenu.vue';
-import fvTabs from './components/fvTabs.vue';
-import fvInput from './components/fvInput.vue';
-import fvSelect from './components/fvSelect.vue';
-
-// Directives
-import fvScroll from './directives/fvScroll.vue';
+import utility from './utility';
 
 const framevuerk = {
-    _dependencies: {
-        moment: null
-    },
+    _dependencies: utility._dependencies,
     use(lib=null){
-        if( lib && lib.name == 'jMoment' ){
+        if( lib && ( lib.name == 'jMoment' || lib.name == 'moment' ) ){
+            utility._dependencies.moment = lib;
             this._dependencies.moment = lib;
         }
     },
     install(Vue){
         // Components
-        Vue.component('fvMain', fvMain);
-        Vue.component('fvContent', fvContent);
-        Vue.component('fvHeader', fvHeader);
-        Vue.component('fvFooter', fvFooter);
-        Vue.component('fvSidebar', fvSidebar);
-        Vue.component('fvSelect', fvSelect);
-        // Vue.component('fvDatepicker', fvDatepicker(this._dependencies.moment) );
-        Vue.component('fvList', fvList);
-        Vue.component('fvMenu', fvMenu);
-        Vue.component('fvInput', fvInput);
-        Vue.component('fvButton', fvButton);
-        // Vue.component('fvFilepicker', fvFilepicker);
-        // Vue.component('fvSwitch', fvSwitch);
-        Vue.component('fvDialog', fvDialog);
-        // Vue.component('fvTable', fvTable);
-        Vue.component('fvTabs', fvTabs);
-        Vue.component('fvRadio', fvRadio);
-        // Vue.component('fvTextarea', fvTextarea);
+        Vue.component('fvMain', require('./components/fvMain.vue').default);
+        Vue.component('fvContent', require('./components/fvContent.vue').default);
+        Vue.component('fvHeader', require('./components/fvHeader.vue').default);
+        Vue.component('fvFooter', require('./components/fvFooter.vue').default);
+        Vue.component('fvSidebar', require('./components/fvSidebar.vue').default);
+        Vue.component('fvSelect', require('./components/fvSelect.vue').default);
+        Vue.component('fvDatepicker', require('./components/fvDatepicker.vue').default);
+        Vue.component('fvList', require('./components/fvList.vue').default);
+        Vue.component('fvMenu', require('./components/fvMenu.vue').default);
+        Vue.component('fvInput', require('./components/fvInput.vue').default);
+        Vue.component('fvButton', require('./components/fvButton.vue').default);
+        Vue.component('fvFilepicker', require('./components/fvFilepicker.vue').default);
+        Vue.component('fvSwitch', require('./components/fvSwitch.vue').default);
+        Vue.component('fvDialog', require('./components/fvDialog.vue').default);
+        Vue.component('fvTable', require('./components/fvTable.vue').default);
+        Vue.component('fvTabs', require('./components/fvTabs.vue').default);
+        Vue.component('fvRadio', require('./components/fvRadio.vue').default);
+        Vue.component('fvTextarea', require('./components/fvTextarea.vue').default);
         // Directives
-        Vue.directive('fvScroll', fvScroll);
+        Vue.directive('fvScroll', require('./directives/fvScroll.vue').default);
     },
     version: pkg.version,
     name: pkg.name
