572bef2c2befcddb19e3301d042cd5f69dc5582b
nainemom <nainemom@gmail.com>
2017-07-06 09:09:08 +0430

    use js events pass and clean-up

--- src/components/fvInput.vue
+++ src/components/fvInput.vue
@@ -1,11 +1,12 @@
 <script>
+    import utility from '../utility';
     export default {
         props: {
             value: {
                 default: null
             },
             displayValue: {
-                default: null
+                default: undefined
             },
             type: {
                 default: 'text'
@@ -26,48 +27,50 @@
                 default: 'fa fa-chevron-down'
             }
         },
-        data: function(){
+        data(){
             return {
                 invalid: false,
                 focus: false
             }
         },
         methods: {
-            checkInvalid: function(){
+            checkInvalid(){
                 if( this.required ){
                     let length = this.value === null? 0: this.value.toString().length;
                     this.invalid = length > 0? true: false;
                 }
             },
-            pFocus: function(){
+            pFocus(){
                 this.$refs.inputEl.focus();
             },
-            pClick: function(event){
+            pClick(event){
                 if( !this.disabled ){
                     this.$emit('click', event);
                 }
             }
         },
         watch: {
-            value: function(){
+            value(){
                 this.checkInvalid();
             }
+        },
+        mounted(){
+            utility.vueEvents().forEach(eventName=>{
+                this.$refs.inputEl.addEventListener(eventName, (event)=> this.$emit(eventName, event) );
+            });
         }
     }
 </script>
 
 <template lang="pug">
-    div.fv-input.fv-input-select(v-if="displayValue",
+    div.fv-input.fv-input-select(v-if="displayValue !== undefined",
         ref="inputEl",
         :class="{invalid: invalid, focus: focus, disabled: disabled}",
         :tabindex="disabled? '': 0",
         :disabled="disabled",
-        @focus="checkInvalid(); $emit('focus', $event); focus = true;",
+        @focus="checkInvalid(); focus = true;",
         @click="pClick($event)",
-        @blur="$emit('blur', $event); focus = false;",
-        @keyup="$emit('keyup', $event)",
-        @keydown="$emit('keydown', $event)",
-        @keypress="$emit('keypress', $event)",
+        @blur="focus = false"
         @invalid.prevent="pFocus()"
     )
         span.placeholder(v-if="typeof displayValue == 'undefined' || displayValue === null || displayValue.length === 0",
@@ -97,13 +100,6 @@
         :value="value",
         :required="required",
         :placeholder="placeholder",
-        @input="$emit('input', $event.target.value)",
-        @focus="$emit('focus', $event)",
-        @click="$emit('click', $event)",
-        @blur="$emit('blur', $event)",
-        @keyup="$emit('keyup', $event)",
-        @keydown="$emit('keydown', $event)",
-        @keypress="$emit('keypress', $event)",
         @invalid.prevent="pFocus()"
     )
 </template>
@@ -123,7 +119,7 @@
             border: 1px solid $shadow-color;
             box-shadow: 0 2px 1px $shadow-color-light;
             border-radius: 0.5em;
-            margin-left: 0.5em;
+            margin-#{$block-end}: 0.5em;
             padding: 0 0.5em;
             line-height: 3em;
         }
