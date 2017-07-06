0c382624a567c1ea4c23c707522f07a509d9e7e2
nainemom <nainemom@gmail.com>
2017-07-06 09:13:20 +0430

    add new parts

--- src/locale/en.js
+++ src/locale/en.js
@@ -16,5 +16,50 @@ export default {
     },
     search(){
         return 'Search...';
+    },
+    momentDisplayFormat(){
+        return 'D MMMM YYYY, HH:mm';
+    },
+    momentFormatPrefix(){
+        return '';
+    },
+    second(){
+        return 'Second';
+    },
+    minute(){
+        return 'Minute';
+    },
+    hour(){
+        return 'Hour';
+    },
+    day(){
+        return 'Day';
+    },
+    week(){
+        return 'Week';
+    },
+    month(){
+        return 'Month';
+    },
+    year(){
+        return 'Year';
+    },
+    browse(){
+        return 'Browse...';
+    },
+    nFileSelected(n=1){
+        if( n == 1){
+            return '1 file selected!';
+        }
+        return `${n} files selected!`;
+    },
+    nextPage(){
+        return 'Next page';
+    },
+    prevPage(){
+        return 'Prev page';
+    },
+    fromN(n=1){
+        return `from ${n}`;
     }
 }
--- src/locale/fa.js
+++ src/locale/fa.js
@@ -16,5 +16,47 @@ export default {
     },
     search(){
         return 'جست‌وجو کنید...';
+    },
+    momentDisplayFormat(){
+        return 'jD jMMMM jYYYY ساعت HH:mm';
+    },
+    momentFormatPrefix(){
+        return 'j';
+    },
+    second(){
+        return 'ثانیه';
+    },
+    minute(){
+        return 'دقیقه';
+    },
+    hour(){
+        return 'ساعت';
+    },
+    day(){
+        return 'روز';
+    },
+    week(){
+        return 'هفته';
+    },
+    month(){
+        return 'ماه';
+    },
+    year(){
+        return 'سال';
+    },
+    browse(){
+        return 'انتخاب کنید...';
+    },
+    nFileSelected(n=1){
+        return `${n} فایل انتخاب شد!`;
+    },
+    nextPage(){
+        return 'صفحه بعد';
+    },
+    prevPage(){
+        return 'صفحه قبل';
+    },
+    fromN(n=1){
+        return `از ${n}`;
     }
 }
