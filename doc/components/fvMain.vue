0824ee9fd7b35efda81ed8f6cb4a01fce349a757
nainemom <nainemom@gmail.com>
2017-07-10 04:14:29 +0430

    add new test document

--- doc/components/fvMain.vue
+++ doc/components/fvMain.vue
@@ -7,7 +7,15 @@
 </script>
 
 <template lang="pug">
-    fv-content salam salam sad taaassa;ammmmmmm
+    fv-content.fv-row
+        div.fv-col-md-12.fv-col-offset-md-1.fv-col-lg-8.fv-col-offset-lg-2
+            p This is main part of framevuerk. The fvHeader, fvSidebar, fvContent and fvFooter components usually uses this container for best styles matching. It`s easy. Look bellow:
+            hr
+            h3 Props:
+            fv-table(local,
+                :fields="['name', 'type', 'default', 'description']",
+                :footer="false",
+                :rows="[{name: 'height', type: 'Number', description: 'Height of viewport'}]")
 </template>
 
 <style lang="scss">
