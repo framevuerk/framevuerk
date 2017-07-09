44ef969cb20568243c201356da114537cc277626
nainemom <nainemom@gmail.com>
2017-07-10 04:12:21 +0430

    linted

--- src/locale/en.js
+++ src/locale/en.js
@@ -1,65 +1,65 @@
 export default {
-  ok () {
-    return 'OK'
-  },
-  cancel () {
-    return 'Cancel'
-  },
-  add (item = '') {
-    return `Add${item ? ` '${item}'` : ''}`
-  },
-  selectNone () {
-    return 'Select none'
-  },
-  notFound () {
-    return 'Not found anything!'
-  },
-  search () {
-    return 'Search...'
-  },
-  momentDisplayFormat () {
-    return 'D MMMM YYYY, HH:mm'
-  },
-  momentFormatPrefix () {
-    return ''
-  },
-  second () {
-    return 'Second'
-  },
-  minute () {
-    return 'Minute'
-  },
-  hour () {
-    return 'Hour'
-  },
-  day () {
-    return 'Day'
-  },
-  week () {
-    return 'Week'
-  },
-  month () {
-    return 'Month'
-  },
-  year () {
-    return 'Year'
-  },
-  browse () {
-    return 'Browse...'
-  },
-  nFileSelected (n = 1) {
-    if (n === 1) {
-      return '1 file selected!'
+    ok() {
+        return 'OK';
+    },
+    cancel() {
+        return 'Cancel';
+    },
+    add(item = '') {
+        return `Add${item ? ` '${item}'` : ''}`;
+    },
+    selectNone() {
+        return 'Select none';
+    },
+    notFound() {
+        return 'Not found anything!';
+    },
+    search() {
+        return 'Search...';
+    },
+    momentDisplayFormat() {
+        return 'D MMMM YYYY, HH:mm';
+    },
+    momentFormatPrefix() {
+        return '';
+    },
+    second() {
+        return 'Second';
+    },
+    minute() {
+        return 'Minute';
+    },
+    hour() {
+        return 'Hour';
+    },
+    day() {
+        return 'Day';
+    },
+    week() {
+        return 'Week';
+    },
+    month() {
+        return 'Month';
+    },
+    year() {
+        return 'Year';
+    },
+    browse() {
+        return 'Browse...';
+    },
+    nFileSelected(n = 1) {
+        if (n === 1) {
+            return '1 file selected!';
+        }
+        return `${n} files selected!`;
+    },
+    nextPage() {
+        return 'Next page';
+    },
+    prevPage() {
+        return 'Prev page';
+    },
+    fromN(n = 1) {
+        return `from ${n}`;
     }
-    return `${n} files selected!`
-  },
-  nextPage () {
-    return 'Next page'
-  },
-  prevPage () {
-    return 'Prev page'
-  },
-  fromN (n = 1) {
-    return `from ${n}`
-  }
-}
+};
