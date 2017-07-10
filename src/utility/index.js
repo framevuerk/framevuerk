bac34878a5440d2e77594cd022ee2bfeb57bb10e
nainemom <nainemom@gmail.com>
2017-07-10 18:42:57 +0430

    linted

--- src/utility/index.js
+++ src/utility/index.js
@@ -1,37 +1,37 @@
 
 export default {
-    isDescendant(parent, child) {
-        if (child === null || parent === null) {
-            return false;
-        }
-        let node = child.parentNode;
-        while (node !== null) {
-            if (node === parent) {
-                return true;
-            }
-            node = node.parentNode;
-        }
-        return false;
-    },
-    contains(text = '', search = '') {
-        return text.toString().toUpperCase().indexOf(search.toString().toUpperCase()) !== -1;
-    },
-    doIt(action) {
-        setTimeout(action, 100);
-    },
-    capitalizeFirstLetter(str = '') {
-        return str.charAt(0).toUpperCase() + str.slice(1);
-    },
-    vueEvents() {
-        return [
-            'click',
-            'focus',
-            'blur',
-            'keyup',
-            'keydown',
-            'keypress',
-            'invalid'
-        ];
-    },
-    _dependencies: {}
-};
+  isDescendant (parent, child) {
+    if (child === null || parent === null) {
+      return false
+    }
+    let node = child.parentNode
+    while (node !== null) {
+      if (node === parent) {
+        return true
+      }
+      node = node.parentNode
+    }
+    return false
+  },
+  contains (text = '', search = '') {
+    return text.toString().toUpperCase().indexOf(search.toString().toUpperCase()) !== -1
+  },
+  doIt (action) {
+    setTimeout(action, 100)
+  },
+  capitalizeFirstLetter (str = '') {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  },
+  vueEvents () {
+    return [
+      'click',
+      'focus',
+      'blur',
+      'keyup',
+      'keydown',
+      'keypress',
+      'invalid'
+    ]
+  },
+  _dependencies: {}
+}
