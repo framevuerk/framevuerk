6b479b8a2102abc7515f07566a07cffb6c78246c
nainemom <nainemom@gmail.com>
2017-07-10 18:43:25 +0430

    linted

--- src/index.js
+++ src/index.js
@@ -1,59 +1,59 @@
-import { name, version } from '../package.json';
-import './style.scss';
-import utility from './utility';
+import { name, version } from '../package.json'
+import './style.scss'
+import utility from './utility'
 
 // Components
-import fvMain from './components/fvMain.vue';
-import fvContent from './components/fvContent.vue';
-import fvHeader from './components/fvHeader.vue';
-import fvFooter from './components/fvFooter.vue';
-import fvSidebar from './components/fvSidebar.vue';
-import fvSelect from './components/fvSelect.vue';
-import fvDatepicker from './components/fvDatepicker.vue';
-import fvList from './components/fvList.vue';
-import fvMenu from './components/fvMenu.vue';
-import fvInput from './components/fvInput.vue';
-import fvButton from './components/fvButton.vue';
-import fvFilepicker from './components/fvFilepicker.vue';
-import fvSwitch from './components/fvSwitch.vue';
-import fvDialog from './components/fvDialog.vue';
-import fvTable from './components/fvTable.vue';
-import fvTabs from './components/fvTabs.vue';
-import fvRadio from './components/fvRadio.vue';
-import fvTextarea from './components/fvTextarea.vue';
+import fvMain from './components/fvMain'
+import fvContent from './components/fvContent'
+import fvHeader from './components/fvHeader'
+import fvFooter from './components/fvFooter'
+import fvSidebar from './components/fvSidebar'
+import fvSelect from './components/fvSelect'
+import fvDatepicker from './components/fvDatepicker'
+import fvList from './components/fvList'
+import fvMenu from './components/fvMenu'
+import fvInput from './components/fvInput'
+import fvButton from './components/fvButton'
+import fvFilepicker from './components/fvFilepicker'
+import fvSwitch from './components/fvSwitch'
+import fvDialog from './components/fvDialog'
+import fvTable from './components/fvTable'
+import fvTabs from './components/fvTabs'
+import fvRadio from './components/fvRadio'
+import fvTextarea from './components/fvTextarea'
 // Directives
-import fvScroll from './directives/fvScroll.vue';
+import fvScroll from './directives/fvScroll'
 
 const framevuerk = {
-    use(lib = null) {
-        if (lib && (lib.name === 'jMoment' || lib.name === 'moment')) {
-            utility._dependencies.moment = lib;
-        }
-    },
-    install(Vue) {
-        // Components
-        Vue.component('fvMain', fvMain);
-        Vue.component('fvContent', fvContent);
-        Vue.component('fvHeader', fvHeader);
-        Vue.component('fvFooter', fvFooter);
-        Vue.component('fvSidebar', fvSidebar);
-        Vue.component('fvSelect', fvSelect);
-        Vue.component('fvDatepicker', fvDatepicker);
-        Vue.component('fvList', fvList);
-        Vue.component('fvMenu', fvMenu);
-        Vue.component('fvInput', fvInput);
-        Vue.component('fvButton', fvButton);
-        Vue.component('fvFilepicker', fvFilepicker);
-        Vue.component('fvSwitch', fvSwitch);
-        Vue.component('fvDialog', fvDialog);
-        Vue.component('fvTable', fvTable);
-        Vue.component('fvTabs', fvTabs);
-        Vue.component('fvRadio', fvRadio);
-        Vue.component('fvTextarea', fvTextarea);
-        // Directives
-        Vue.directive('fvScroll', fvScroll);
-    },
-    version,
-    name
-};
-module.exports = framevuerk;
+  use (lib = null) {
+    if (lib && (lib.name === 'jMoment' || lib.name === 'moment')) {
+      utility._dependencies.moment = lib
+    }
+  },
+  install (Vue) {
+    // Components
+    Vue.component('fvMain', fvMain)
+    Vue.component('fvContent', fvContent)
+    Vue.component('fvHeader', fvHeader)
+    Vue.component('fvFooter', fvFooter)
+    Vue.component('fvSidebar', fvSidebar)
+    Vue.component('fvSelect', fvSelect)
+    Vue.component('fvDatepicker', fvDatepicker)
+    Vue.component('fvList', fvList)
+    Vue.component('fvMenu', fvMenu)
+    Vue.component('fvInput', fvInput)
+    Vue.component('fvButton', fvButton)
+    Vue.component('fvFilepicker', fvFilepicker)
+    Vue.component('fvSwitch', fvSwitch)
+    Vue.component('fvDialog', fvDialog)
+    Vue.component('fvTable', fvTable)
+    Vue.component('fvTabs', fvTabs)
+    Vue.component('fvRadio', fvRadio)
+    Vue.component('fvTextarea', fvTextarea)
+    // Directives
+    Vue.directive('fvScroll', fvScroll)
+  },
+  version,
+  name
+}
+module.exports = framevuerk
--- src/locale/fa.js
+++ src/locale/fa.js
@@ -1,62 +1,62 @@
 export default {
-    ok() {
-        return 'ثبت';
-    },
-    cancel() {
-        return 'انصراف';
-    },
-    add(item = '') {
-        return `اضافه کردن${item ? ` '${item}'` : ''}`;
-    },
-    selectNone() {
-        return 'حذف انتخاب';
-    },
-    notFound() {
-        return 'چیزی پیدا نشد...';
-    },
-    search() {
-        return 'جست‌وجو کنید...';
-    },
-    momentDisplayFormat() {
-        return 'jD jMMMM jYYYY ساعت HH:mm';
-    },
-    momentFormatPrefix() {
-        return 'j';
-    },
-    second() {
-        return 'ثانیه';
-    },
-    minute() {
-        return 'دقیقه';
-    },
-    hour() {
-        return 'ساعت';
-    },
-    day() {
-        return 'روز';
-    },
-    week() {
-        return 'هفته';
-    },
-    month() {
-        return 'ماه';
-    },
-    year() {
-        return 'سال';
-    },
-    browse() {
-        return 'انتخاب کنید...';
-    },
-    nFileSelected(n = 1) {
-        return `${n} فایل انتخاب شد!`;
-    },
-    nextPage() {
-        return 'صفحه بعد';
-    },
-    prevPage() {
-        return 'صفحه قبل';
-    },
-    fromN(n = 1) {
-        return `از ${n}`;
-    }
-};
+  ok () {
+    return 'ثبت'
+  },
+  cancel () {
+    return 'انصراف'
+  },
+  add (item = '') {
+    return `اضافه کردن${item ? ` '${item}'` : ''}`
+  },
+  selectNone () {
+    return 'حذف انتخاب'
+  },
+  notFound () {
+    return 'چیزی پیدا نشد...'
+  },
+  search () {
+    return 'جست‌وجو کنید...'
+  },
+  momentDisplayFormat () {
+    return 'jD jMMMM jYYYY ساعت HH:mm'
+  },
+  momentFormatPrefix () {
+    return 'j'
+  },
+  second () {
+    return 'ثانیه'
+  },
+  minute () {
+    return 'دقیقه'
+  },
+  hour () {
+    return 'ساعت'
+  },
+  day () {
+    return 'روز'
+  },
+  week () {
+    return 'هفته'
+  },
+  month () {
+    return 'ماه'
+  },
+  year () {
+    return 'سال'
+  },
+  browse () {
+    return 'انتخاب کنید...'
+  },
+  nFileSelected (n = 1) {
+    return `${n} فایل انتخاب شد!`
+  },
+  nextPage () {
+    return 'صفحه بعد'
+  },
+  prevPage () {
+    return 'صفحه قبل'
+  },
+  fromN (n = 1) {
+    return `از ${n}`
+  }
+}
