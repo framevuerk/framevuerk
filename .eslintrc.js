20171487007a943de45a250e3395f26e8610e599
nainemom <nainemom@gmail.com>
2017-07-10 18:42:11 +0430

    updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,54 +1,7 @@
 module.exports = {
-  extends: [
-    'xo/esnext',
-    'plugin:vue/recommended',
-    'plugin:unicorn/recommended'
-  ],
-  plugins: [
-    'html',
-    'unicorn'
-  ],
-  env: {
-    'browser': true,
-    'node': true
-  },
-  settings: {
-    'html/html-extensions': ['.vue', '.html'],
-    'html/indent': '+4',
-    'html/report-bad-indent': 'error',
-  },
-  rules: {
-    indent: ['error', 4],
-    'unicorn/filename-case': ['off'],
-    'unicorn/no-abusive-eslint-disable': ['off'],
-    'object-curly-spacing': ['error', 'always'],
-    'vue/html-end-tags': 'error',
-    'vue/html-no-self-closing': 'error',
-    'vue/no-confusing-v-for-v-if': 'error',
-    'vue/no-duplicate-attributes': 'error',
-    'vue/no-textarea-mustache': 'error',
-    'vue/order-in-components': 'error',
-    'vue/require-component-is': 'error',
-    'vue/require-v-for-key': 'error',
-    'vue/html-quotes': 'error',
-    'vue/v-bind-style': 'error',
-    'vue/v-on-style': 'error',
-    'vue/jsx-uses-vars': 'error',
-    'vue/no-invalid-template-root': 'error',
-    'vue/no-invalid-v-bind': 'error',
-    'vue/no-invalid-v-cloak': 'error',
-    'vue/no-invalid-v-else-if': 'error',
-    'vue/no-invalid-v-else': 'error',
-    'vue/no-invalid-v-for': 'error',
-    'vue/no-invalid-v-html': 'error',
-    'vue/no-invalid-v-if': 'error',
-    'vue/no-invalid-v-model': 'error',
-    'vue/no-invalid-v-on': 'error',
-    'vue/no-invalid-v-once': 'error',
-    'vue/no-invalid-v-pre': 'error',
-    'vue/no-invalid-v-show': 'error',
-    'vue/no-invalid-v-text': 'error',
-    'vue/no-parsing-error': 'error',
-    'vue/no-template-key': 'error'
-  }
-}
+    extends: 'standard',
+    env: {
+        browser: true,
+        node: true
+    }
+};
