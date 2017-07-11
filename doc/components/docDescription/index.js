c411b03b4bc2079e5c4deb5a03ad5c79ff10e578
nainemom <nainemom@gmail.com>
2017-07-11 18:42:24 +0430

    created

---
+++ doc/components/docDescription/index.js
@@ -0,0 +1,5 @@
+import template from './template.pug'
+
+export default {
+  render: template.render
+} 
---
+++ doc/pages/main/index.js
@@ -0,0 +1,34 @@
+import template from './template.pug'
+import style from './style.scss'
+
+export default {
+  computed: {
+    sidebarItems(){
+      const ret = [];
+      const components = [
+        'fvMain'
+      ]
+      components.forEach(component=>{
+        ret.push({
+          key: component,
+          icon: 'fa fa-puzzle-piece',
+          text: ` ${component}`,
+          selected: this.$route.name === component,
+          action: ()=>{
+            this.$router.push(`/components/${component}`);
+          }
+        });
+      });
+      return ret;
+    },
+    sidebarTabs(){
+      return [
+        {title: 'Components', slot: 'components'},
+        {title: 'Directives', slot: 'directives'},
+        {title: 'Styles', slot: 'styles'}
+      ]
+    }
+  },
+  style,
+  render: template.render
+} 
