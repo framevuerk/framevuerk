dc7fb141efca2c6a607df816c9e2a68597cfc175
nainemom <nainemom@gmail.com>
2017-07-12 17:43:41 +0430

    add fvHeader docs

---
+++ docs-src/pages/component_fvHeader/index.js
@@ -0,0 +1,15 @@
+import template from './template.pug'
+import docApi from '../../components/docApi'
+import docDescription from '../../components/docDescription'
+import docExample from '../../components/docExample'
+import docCode from '../../components/docCode'
+
+export default {
+  components: {
+    docApi,
+    docDescription,
+    docExample,
+    docCode
+  },
+  render: template.render
+}
