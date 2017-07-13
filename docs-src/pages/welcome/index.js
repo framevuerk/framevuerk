10cc971fe2217cc7163cc0c75212ed0da0caf924
nainemom <nainemom@gmail.com>
2017-07-13 18:26:43 +0430

    add welcome page

---
+++ docs-src/pages/welcome/index.js
@@ -0,0 +1,8 @@
+import template from './template.pug'
+import style from './style.scss'
+import './assets/github.svg'
+import './assets/npm.svg'
+export default {
+  render: template.render,
+  style
+}
