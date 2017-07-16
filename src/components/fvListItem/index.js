3b00d6d6ed76bbafc5bb4eec837c5d3b99c11e54
nainemom <nainemom@gmail.com>
2017-07-16 16:33:37 +0430

    add fvListItem

---
+++ src/components/fvListItem/index.js
@@ -0,0 +1,71 @@
+import utility from '../../utility'
+import template from './template.pug'
+import style from './style.scss'
+
+export default {
+  props: {
+    icon: {
+      type: String,
+      default: ''
+    },
+    text: {
+      type: String,
+      default: ''
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    selected: {
+      type: Boolean,
+      default: false
+    },
+    highlighted: {
+      type: Boolean,
+      default: false
+    },
+    expanded: {
+      type: Boolean,
+      default: false
+    },
+    items: {
+      default: []
+    }
+  },
+  data() {
+    return {
+      pExpanded: this.expanded,
+      animationName: ''
+    }
+  },
+  computed: {
+    hasItems() {
+      return this.items && this.items.length;
+    }
+  },
+  methods: {
+    click() {
+      if( !this.disabled ){
+        this.$emit('click')
+        if( this.hasItems ) {
+          this.pExpanded = !this.pExpanded;
+        }
+      }
+    }
+  },
+  mounted() {
+    if( this.hasItems ){
+      this.items.forEach(item=>{
+        if( item.selected ){
+          this.pExpanded = true;
+        }
+      });
+      utility.doIt(()=>{
+        this.animationName = 'fv-list-expand';
+      })
+      
+    }
+  },
+  style,
+  render: template.render
+}
