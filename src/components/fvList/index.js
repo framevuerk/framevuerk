de0043642078d13949789d425f210fd3957e2226
nainemom <nainemom@gmail.com>
2017-07-17 00:51:21 +0430

    add focused style

--- src/components/fvList/index.js
+++ src/components/fvList/index.js
@@ -15,7 +15,8 @@ export default {
   },
   data () {
     return {
-      highlightedOption: null
+      highlightedOption: this.items.length ? 0 : null,
+      focused: false
     }
   },
   methods: {
