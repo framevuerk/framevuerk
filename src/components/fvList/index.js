fde8b46ce1f243dd0e474ce41d60c2d3c4bf6e67
nainemom <nainemom@gmail.com>
2017-07-17 20:58:45 +0430

    add searchQuery and refactored

--- src/components/fvList/index.js
+++ src/components/fvList/index.js
@@ -1,3 +1,5 @@
+import utility from '../../utility'
+import locale from 'locale'
 import template from './template.pug'
 import style from './style.scss'
 /* global CONFIG */
@@ -11,12 +13,25 @@ export default {
     expanded: {
       type: Boolean,
       default: false
+    },
+    searchQuery: {
+      type: String,
+      default: ''
+    },
+    selected: {
+      default: undefined
     }
   },
   data () {
     return {
       highlightedOption: this.items.length ? 0 : null,
-      focused: false
+      focused: false,
+      locale
+    }
+  },
+  computed: {
+    pItems () {
+      return this.items.filter(this.equalSearch)
     }
   },
   methods: {
@@ -25,8 +40,23 @@ export default {
       if (item.action) {
         item.action()
       }
-      if (!item.disabled && index !== false) {
+      if (index !== false) {
         this.highlightedOption = index
+      }
+    },
+    isSelected (item) {
+      const value = item.value || item || ''
+      if (item.selected) {
+        return true
+      }
+      if (typeof this.selected === 'undefined' || this.selected === null) {
+        return false
+      } else {
+        if (this.selected.constructor === Array) {
+          return this.selected.indexOf(value) !== -1
+        } else {
+          return this.selected === value
+        }
       }
     },
     pKeyDown (event) {
@@ -35,21 +65,21 @@ export default {
       }
       switch (event.which) {
       case 38: // up
-        this.highlightedOption = this.highlightedOption == null ? this.items.length : this.highlightedOption
-        this.highlightedOption = this.highlightedOption - 1 < 0 ? this.items.length - 1 : this.highlightedOption - 1
+        this.highlightedOption = this.highlightedOption == null ? this.pItems.length : this.highlightedOption
+        this.highlightedOption = this.highlightedOption - 1 < 0 ? this.pItems.length - 1 : this.highlightedOption - 1
         break
       case 40: // down
         this.highlightedOption = this.highlightedOption == null ? -1 : this.highlightedOption
-        this.highlightedOption = this.highlightedOption + 1 >= this.items.length ? 0 : this.highlightedOption + 1
+        this.highlightedOption = this.highlightedOption + 1 >= this.pItems.length ? 0 : this.highlightedOption + 1
         break
       case CONFIG.DIRECTION === 'ltr' ? 37 : 39: // 37: left, 39: right,
         if (this.highlightedOption !== null) {
-          this.$refs.items[this.highlightedOption].collapse()
+          this.$refs.pItems[this.highlightedOption].collapse()
         }
         break
       case CONFIG.DIRECTION === 'ltr' ? 39 : 37: // 37: left, 39: right,
         if (this.highlightedOption !== null) {
-          this.$refs.items[this.highlightedOption].expand()
+          this.$refs.pItems[this.highlightedOption].expand()
         }
         break
       case 13: // enter
@@ -58,6 +88,19 @@ export default {
           this.clickItem(this.items[ this.highlightedOption ])
         }
         break
+      }
+    },
+    equalSearch (item) {
+      const pItem = {
+        text: item.text || item || '',
+        value: item.value || item || ''
+      }
+      if (!this.searchQuery ||
+        utility.contains(pItem.text, this.searchQuery) ||
+        utility.contains(pItem.value, this.searchQuery)) {
+        return true
+      } else {
+        return false
       }
     }
   },
