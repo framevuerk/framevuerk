8418ab26251a05ad63982f691fd973e21c077a9e
nainemom <nainemom@gmail.com>
2017-07-25 21:46:08 +0430

    removed

--- docs-src/pages/welcome/index.js
+++
@@ -1,9 +0,0 @@
-import template from './template.pug'
-import style from './style.scss'
-import '../../assets/github.svg'
-import '../../assets/npm.svg'
-import '../../assets/logo.svg'
-export default {
-  render: template.render,
-  style
-}
