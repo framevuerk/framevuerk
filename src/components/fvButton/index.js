fcca53e1546fbb839cada3f02240614cc24769c5
nainemom <nainemom@gmail.com>
2017-07-31 17:19:58 +0430

    add loading state

--- src/components/fvButton/index.js
+++ src/components/fvButton/index.js
@@ -1,6 +1,12 @@
 import template from './template.pug'
 import style from './style.scss'
 export default {
+  props: {
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  },
   style,
   render: template.render
 }
