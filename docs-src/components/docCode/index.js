2fec95c23ec4dae8db68d170e001ed41fb43718d
nainemom <nainemom@gmail.com>
2017-10-14 19:07:24 +0330

    use vue-highlightjs

--- docs-src/components/docCode/index.js
+++ docs-src/components/docCode/index.js
@@ -2,10 +2,17 @@ import template from './template.pug'
 import style from './style.scss'
 
 export default {
+  data(){
+    return {
+      highlightjs: '',
+      v: ''
+    }
+  },
   props: {
     title: String,
     lang: {
-      type: String
+      type: String,
+      default: 'html'
     }
   },
   style,
