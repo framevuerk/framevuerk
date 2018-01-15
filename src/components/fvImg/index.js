34d8d2cc4f4defca3518a7b32ec286bf1b89b836
nainemom <nainemom@gmail.com>
2018-01-15 18:26:01 +0330

    fully updated: use fvSlider

--- src/components/fvImg/index.js
+++ src/components/fvImg/index.js
@@ -1,33 +1,20 @@
-import utility from '../../utility'
 import template from './template.pug'
 import style from './style.scss'
-import fvButton from '../fvButton'
+import fvSlider from '../fvSlider'
 /* global process */
 
 export default {
   components: {
-    fvButton
+    fvSlider
   },
   props: {
-    value: {
-      type: Number,
-      default: 0
-    },
     src: {
       type: [Array, String],
       default: () => []
     },
-    width: {
-      type: [Number, String],
-      default: '100%'
-    },
-    height: {
-      type: [Number, String],
-      default: '25em'
-    },
-    nav: {
+    showButtons: {
       type: Boolean,
-      default: true
+      default: false
     },
     interval: {
       type: Number,
@@ -36,137 +23,15 @@ export default {
   },
   data () {
     return {
-      hammer: undefined,
-      index: this.value || 0,
-      timer: null,
-      animationName: 'fv-img-prev',
-      dirs: {
-        next: process.env.DIRECTION === 'ltr' ? 'left' : 'right',
-        prev: process.env.DIRECTION === 'ltr' ? 'right' : 'left'
-      }
+      value: undefined
     }
   },
   computed: {
-    size () {
-      return {
-        width: typeof this.width === 'number' ? `${this.width}px` : this.width,
-        height: typeof this.height === 'number' ? `${this.height}px` : this.height
-      }
-    },
-    pNav () {
-      if (!this.nav || this.images.length < 2) {
-        return false
-      }
-      return true
-    },
     images () {
-      const ret = []
       if (typeof this.src === 'string') {
-        ret.push(this.imageObj(this.src))
-      } else {
-        this.src.forEach(src => {
-          ret.push(this.imageObj(src))
-        })
+        return [this.src]
       }
-      return ret
-    },
-    nextPageBtnContent () {
-      if (process.env.DIRECTION === 'rtl') {
-        return `<i class="fa fa-chevron-left"></i>`
-      } else {
-        return `<i class="fa fa-chevron-right"></i>`
-      }
-    },
-    prevPageBtnContent () {
-      if (process.env.DIRECTION === 'rtl') {
-        return `<i class="fa fa-chevron-right"></i>`
-      } else {
-        return `<i class="fa fa-chevron-left"></i>`
-      }
-    }
-  },
-  methods: {
-    imageObj (src) {
-      return {
-        image: typeof src === 'string' ? src : src.image || '',
-        caption: {
-          text: typeof src === 'string' ? '' : (src.caption ? src.caption.text || src.caption || src.caption.text : ''),
-          class: typeof src === 'string' ? 'bottom' : (src.caption ? src.caption.class || 'bottom' : 'bottom')
-        }
-      }
-    },
-    focus () {
-    },
-    getUrl (src) {
-      return `url("${src}")`
-    },
-    moveIndex (num = 1) {
-      const newIndex = this.index + num
-      if (newIndex >= this.images.length) {
-        this.index = 0
-      } else if (newIndex < 0) {
-        this.index = this.images.length - 1
-      } else {
-        this.index = newIndex
-      }
-    },
-    initerval () {
-      clearInterval(this.timer)
-      if (this.interval > 0 && this.images.length > 1) {
-        this.timer = setInterval(() => {
-          this.moveIndex(1)
-        }, this.interval)
-      } else {
-        clearInterval(this.timer)
-      }
-    },
-    initHammer () {
-      if (this.hammer && this.images.length > 1) {
-        const mc = new this.hammer.Manager(this.$el, {
-          recognizers: [
-            [this.hammer.Swipe, { direction: this.hammer.DIRECTION_HORIZONTAL }]
-          ]
-        })
-        mc.on(`swipe${this.dirs.next}`, () => {
-          this.moveIndex(1)
-        })
-        mc.on(`swipe${this.dirs.prev}`, () => {
-          this.moveIndex(-1)
-        })
-      }
-    }
-  },
-  created () {
-    this.hammer = utility._dependencies.hammer
-  },
-  mounted () {
-    this.initerval()
-    this.initHammer()
-  },
-  watch: {
-    interval () {
-      this.initerval()
-    },
-    index (newIndex, oldIndex) {
-      this.$emit('input', newIndex)
-      const lastIndex = this.images.length - 1
-      if (newIndex === lastIndex && oldIndex === 0) {
-        this.animationName = `fv-img-${this.dirs.prev}`
-      } else if (newIndex > oldIndex || (newIndex === 0 && oldIndex === this.images.length - 1)) {
-        this.animationName = `fv-img-${this.dirs.next}`
-      } else {
-        this.animationName = `fv-img-${this.dirs.prev}`
-      }
-      this.initerval()
-    },
-    src () {
-      this.initerval()
-      this.initHammer()
-    },
-    value () {
-      if (this.value !== this.index) {
-        this.index = this.value
-      }
+      return this.src
     }
   },
   style,
