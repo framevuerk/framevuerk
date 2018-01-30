e64d13b23d57b55b0b276ade4c47af008c37d05e
nainemom <nainemom@gmail.com>
2018-01-30 22:18:25 +0330

    add build script

---
+++ build/index.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+const webpack = require('webpack')
+const config = require('./webpack.config.js')
+const configDocs = require('./webpack.config.docs.js')
+
+function build (part) {
+  return new Promise((resolve, reject) => {
+    switch (part) {
+      case 'docs':
+        webpack(configDocs, (err, stats) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve(stats)
+          }
+        })
+        break
+      case 'lib':
+      default:
+        webpack(config, (err, stats) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve(stats)
+          }
+        })
+    }
+  })
+}
+const tasks = []
+if (process.argv.length < 3) {
+  tasks.push(build('lib'))
+  tasks.push(build('docs'))
+} else {
+  tasks.push(build(process.argv[2]))
+}
+
+console.log('Building Framevuerk...')
+Promise.all(tasks).then((stats) => {
+  console.log('Done!')
+}).catch((err) => {
+  throw new Error(err)
+})
