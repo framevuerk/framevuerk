6fce1a1b0601a9fe8ffc906584ddd96a3338f3a0
nainemom <nainemom@gmail.com>
2018-02-03 16:59:05 +0330

    add & use prerender-spa-plugin

--- build/webpack.config.docs.js
+++ build/webpack.config.docs.js
@@ -3,20 +3,25 @@ const pkg = require(path.resolve(__dirname, '../package.json'))
 const config = require(path.resolve(__dirname, '../.framevuerkrc.json'))
 const utils = require(path.resolve(__dirname, './utils.js'))
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
+const PrerenderSpaPlugin = require('prerender-spa-plugin')
+const routes = require(utils.resolve('../docs-src/routes.js'))('config')
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-
+const plugins = [new HtmlWebpackPlugin({
+  title: 'Framevuerk Documents',
+  filename: utils.resolve('../docs/index.html'),
+  template: utils.resolve('../docs-src/index.html')
+})]
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(new PrerenderSpaPlugin(
+    utils.resolve('../docs'),
+    routes.map(route => route.path)
+  ))
+}
 module.exports = utils.generateConfig(Object.assign(config, {
   name: `${pkg.name}-docs`,
   version: pkg.version,
   entry: utils.resolve('../docs-src/index.js'),
   path: utils.resolve('../docs'),
   minify: process.env.NODE_ENV === 'production',
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Framevuerk Documents',
-      filename: 'doc.html',
-      template: utils.resolve('../docs-src/doc.html')
-    })
-  ]
+  plugins: plugins
 }))
