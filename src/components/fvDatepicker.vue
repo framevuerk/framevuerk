c7d77691e87c377bb6ae9fae01f153393217e106
nainemom <nainemom@gmail.com>
2018-02-13 14:26:28 +0330

    redesigned

--- src/components/fvDatepicker.vue
+++ src/components/fvDatepicker.vue
@@ -12,55 +12,53 @@ span
     ref="inputEl")
   fv-dialog.fv-datepicker(ref="dialog",
     :class="dialogClass",
-    position="center",
+    :left.sync="dialogPosition.left",
+    :top.sync="dialogPosition.top",
+    width="300px",
     @close="$emit('close')",
     @open="$emit('open')",
-    :first-focus-on="true",
+    :first-focus-on="false",
     :auto-close="false")
-    fv-content.fv-no-padding
-      .fv-row
-        section4(v-for="(val, index) in pick",
-          :class="{'fv-col-4': pick.length % 3 === 0, 'fv-col-6': pick.length % 3 !== 0 && pick.length % 2 === 0, 'fv-col-xs-12': pick.length === 1}",
-          :key="index")
-          label.fv-control-label.fv-text-center(v-html="locale[val]()")
-          fv-input.fv-block.fv-lg.fv-text-center(ref="input",
-            :value="pValue?  pValue.format(locale.momentDisplayFormat(val)): ''",
-            @blur.native="$event.target.value = pValue.format(locale.momentDisplayFormat(val))",
-            @input="pSet(val, $event)",
-            required,
-            type="number")
-    fv-footer
-      .fv-grow
-      fv-button(icon="fa fa-times",
-        :text="locale.cancel()",
-        @click="close()")
-      fv-button(v-if="value",
-        icon="fa fa-circle-o",
-        :text="locale.clear()",
-        @click="$emit('input', undefined); close();")
-      fv-button.fv-ok(icon="fa fa-check",
-        :text="locale.ok()",
-        @click="$emit('input', pValue.toDate()); close();")
+    fv-header.header(height="4em", tabindex="0", @keydown.native="onHeaderKeydown")
+      .fv-input-group.header-buttons
+        fv-button.fv-sm.text-focus(@click="moveValue('year', -1)", :icon="icons.prevYear", tabindex="-1")
+        fv-button.fv-sm.text-focus(@click="moveValue('month', -1, false)", :icon="icons.prevMonth", tabindex="-1")
+      .title.fv-text-center
+        h4 {{visualProps.month}}/{{visualProps.year}}
+      .fv-input-group.header-buttons
+        fv-button.fv-sm.text-focus(@click="moveValue('month', 1, false)", :icon="icons.nextMonth", tabindex="-1")
+        fv-button.fv-sm.text-focus(@click="moveValue('year', 1)", :icon="icons.nextYear", tabindex="-1")
+    fv-content.content(tabindex="0", @keydown.native="onContentKeydown")
+      table.days-table
+        thead(v-if="PersianDate")
+          tr
+            th(v-for="wd in editingValue.rangeName().weekdaysMin", v-html="wd")
+        tbody
+          tr(v-for="dp in [0, 7, 14, 21, 28]")
+            td(v-for="d in 7",
+              @click="setValue('date', (d + dp) - visualProps.monthFirstDay, true)",
+              :disabled="(d + dp) <= visualProps.monthFirstDay || (d + dp) - visualProps.monthFirstDay > visualProps.daysInMonth",
+              :class="{highlighted: isHighlighted((d + dp) - visualProps.monthFirstDay), selected: isSelected((d + dp) - visualProps.monthFirstDay)}") {{ (d + dp) > visualProps.monthFirstDay && (d + dp) - visualProps.monthFirstDay <= visualProps.daysInMonth ? (d + dp) - visualProps.monthFirstDay : ''}}
 </template>
 
 <script>
+import utility from '../utility'
 import locale from 'locale'
 import fvMain from './fvMain.vue'
 import fvContent from './fvContent.vue'
 import fvDialog from './fvDialog.vue'
 import fvInput from './fvInput.vue'
-import fvButton from './fvButton.vue'
 
 export default {
   components: {
     fvMain,
     fvContent,
     fvDialog,
-    fvInput,
-    fvButton
+    fvInput
   },
   props: {
     value: {
+      type: Date,
       default: undefined
     },
     inputClass: {
@@ -77,69 +75,92 @@ export default {
       type: Boolean,
       default: false
     },
-    pick: {
-      type: Array,
-      validator: (v) => {
-        for (let i = 0; i < v.length; i++) {
-          if ([
-            'year',
-            'month',
-            'day',
-            'hour',
-            'minute',
-            'second'
-          ].indexOf(v[i]) === -1) {
-            return false
-          }
-        }
-        return true
-      },
-      default: () => ['year', 'month', 'day', 'hour', 'minute', 'second']
-    },
     placeholder: {
       type: String,
       default: ''
     },
     displayFormat: {
-      default: locale.momentDisplayFormat()
+      default: locale.dateDisplayFormat()
+    },
+    calendar: {
+      type: String,
+      default: process.env.locale === 'fa' ? 'persian' : 'gregorian'
+    },
+    locale: {
+      type: String,
+      default: process.env.locale
     }
   },
   data () {
     return {
-      locale,
-      moment: undefined, // will set on created
-      pValue: undefined
+      PersianDate: null, // will set on created
+      editingValue: undefined,
+      visualProps: {},
+      dialogPosition: {},
+      highlightedDate: null
     }
   },
   created () {
-    this.moment = require('../').dependencies.moment
-    if (this.moment) {
-      if (process.env.locale === 'fa') {
-        this.moment.loadPersian()
-      }
-    }
+    this.PersianDate = require('../').dependencies['persian-date']
+    this.setEditingValue()
   },
   computed: {
     fvValidate () {
       return this.$refs.inputEl.fvValidate || false
     },
     displayValue () {
-      if (this.value && this.moment) {
-        return this.moment.utc(this.value).format(this.displayFormat)
-      } else {
-        return undefined
+      if (this.value && this.PersianDate) {
+        const value = new this.PersianDate(this.value).toCalendar(this.calendar).toLocale(this.locale)
+        return value.format(this.displayFormat)
+      } else if (this.value) {
+        return this.value.toDateString()
+      }
+      return undefined
+    },
+    icons () {
+      return {
+        nextMonth: `fa fa-angle-${process.env.direction === 'ltr' ? 'right' : 'left'}`,
+        prevMonth: `fa fa-angle-${process.env.direction === 'rtl' ? 'right' : 'left'}`,
+        nextYear: `fa fa-angle-double-${process.env.direction === 'ltr' ? 'right' : 'left'}`,
+        prevYear: `fa fa-angle-double-${process.env.direction === 'rtl' ? 'right' : 'left'}`,
       }
     }
   },
   methods: {
-    open () {
-      let date
-      if (this.value && this.value.constructor === Date) {
-        date = this.value
+    setEditingValue () {
+      if (this.PersianDate) {
+        this.editingValue = this.value ? new this.PersianDate(this.value).toCalendar(this.calendar).toLocale(this.locale) : new this.PersianDate().toCalendar(this.calendar).toLocale(this.locale)
       } else {
-        date = new Date()
+        this.editingValue = this.value ? new Date(this.value.getTime()) : new Date
       }
-      this.pValue = this.moment.utc(date)
+    },
+    calcVisualProps () {
+      this.visualProps = {
+        year: this.PersianDate ? this.editingValue.year(): this.editingValue.getFullYear(),
+        month: this.PersianDate ? this.editingValue.month(): this.editingValue.getMonth() + 1,
+        date: this.PersianDate ? this.editingValue.date(): this.editingValue.getDate(),
+        monthFirstDay: this.PersianDate ? new this.PersianDate(this.editingValue).toCalendar(this.calendar).toLocale(this.locale).startOf('month').day() % 7 : new Date(this.editingValue.getFullYear(), this.editingValue.getMonth(), 1).getDay(),
+        daysInMonth: this.PersianDate ? this.editingValue.daysInMonth() : new Date(this.editingValue.getFullYear(), this.editingValue.getMonth() + 1, 0).getDate()
+      }
+      this.$forceUpdate()
+      return this.visualProps
+    },
+    open () {
+      this.setEditingValue()
+      this.calcVisualProps()
+      
+      const main = utility.fvParent(this, 'fv-main')
+      const offset = utility.offsetTo(this.$refs.inputEl.$el, main.$el)
+      let content = utility.fvParent(this, 'fv-content')
+      while (content) {
+        offset.top -= content.$el.scrollTop
+        content = utility.fvParent(content, 'fv-content')
+      }
+      this.dialogPosition = {
+        left: `${offset.left}px`,
+        top: `${offset.top}px`
+      }
+
       this.$refs.dialog.open()
     },
     close () {
@@ -148,18 +169,216 @@ export default {
     focus () {
       this.$refs.inputEl.$el.focus()
     },
-    pSet (unit, value) {
-      const oldValue = parseInt(this.pValue.format(locale.momentDisplayFormat(unit)))
-      const newValue = parseInt(value)
-      if (newValue > oldValue) {
-        this.pValue.add(newValue - oldValue, `${unit}s`)
-      } else if (newValue < oldValue) {
-        this.pValue.subtract(oldValue - newValue, `${unit}s`)
-      } else {
-        this.pValue.subtract(0, `${unit}s`)
+    isSelected(date) {
+      if (!this.value) {
+        return false
       }
-      this.$forceUpdate()
+      if (this.PersianDate) {
+        const value = new this.PersianDate(this.value).toCalendar(this.calendar).toLocale(this.locale)
+        return value.date() === date && value.month() === this.editingValue.month() && value.year() === this.editingValue.year()
+      } else {
+        return this.value.getDate() === date && this.value.getMonth() === this.editingValue.getMonth() && this.value.getFullYear() === this.editingValue.getFullYear()
+      }
+      return false
+    },
+    isHighlighted(date) {
+      if (!this.editingValue) {
+        return false
+      }
+      if (this.PersianDate) {
+        return this.editingValue.date() === date
+      } else {
+        return this.editingValue.getDate() === date
+      }
+      return false
+    },
+    moveValue (unit, value, emit = false){
+      let ret
+      if (this.PersianDate) {
+        switch (unit) {
+          case 'year':
+            this.editingValue.year(this.editingValue.year() + value)
+            break
+          case 'month':
+            this.editingValue.month(this.editingValue.month() + value)
+            break
+          case 'date':
+            this.editingValue.date(this.editingValue.date() + value)
+            break
+        }
+        ret = this.editingValue.toDate()
+      } else {
+        switch (unit) {
+          case 'year':
+            this.editingValue.setFullYear(this.editingValue.getFullYear() + value)
+            break
+          case 'month':
+            this.editingValue.setMonth(this.editingValue.getMonth() + value)
+            break
+          case 'date':
+            this.editingValue.setDate(this.editingValue.getDate() + value)
+            break
+        }
+        ret = new Date(this.editingValue)
+      }
+      if (emit) {
+        this.$emit('input', this.value && ret.toString() === this.value.toString() ? undefined : ret )
+        this.close()
+      }
+      this.calcVisualProps()
+    },
+    setValue (unit, value, emit = false) {
+      let ret
+      if (this.PersianDate) {
+        switch (unit) {
+          case 'year':
+            this.editingValue.year(value)
+            break
+          case 'month':
+            this.editingValue.month(value)
+            break
+          case 'date':
+            this.editingValue.date(value)
+            break
+        }
+        ret = this.editingValue.toDate()
+      } else {
+        switch (unit) {
+          case 'year':
+            this.editingValue.setFullYear(value)
+            break
+          case 'month':
+            this.editingValue.setMonth(value)
+            break
+          case 'date':
+            this.editingValue.setDate(value)
+            break
+        }
+        ret = new Date(this.editingValue)
+      }
+      if (emit) {
+        this.$emit('input', this.value && ret.toString() === this.value.toString() ? undefined : ret )
+        this.close()
+      }
+      this.calcVisualProps()
+    },
+    onContentKeydown (event) {
+      switch (event.which) {
+        case 38: // up
+          event.preventDefault()
+          this.moveValue('date', -7)
+          break
+        case 40: // down
+          event.preventDefault()
+          this.moveValue('date', 7)
+          break
+        case process.env.direction === 'ltr' ? 37 : 39: // 37: left, 39: right,
+          event.preventDefault()
+          this.moveValue('date', -1)
+          break
+        case process.env.direction === 'ltr' ? 39 : 37: // 37: left, 39: right,
+          event.preventDefault()
+          this.moveValue('date', 1)
+          break
+        case 13: // enter
+          event.preventDefault()
+          this.moveValue('date', 0, true)
+          break
+      }
+      this.calcVisualProps()
+    },
+    onHeaderKeydown (event) {
+      switch (event.which) {
+        case 38: // up
+          event.preventDefault()
+          this.moveValue('year', -1)
+          break
+        case 40: // down
+          event.preventDefault()
+          this.moveValue('year', 1)
+          break
+        case process.env.direction === 'ltr' ? 37 : 39: // 37: left, 39: right,
+          event.preventDefault()
+          this.moveValue('month', -1)
+          break
+        case process.env.direction === 'ltr' ? 39 : 37: // 37: left, 39: right,
+          event.preventDefault()
+          this.moveValue('month', 1)
+          break
+      }
     }
   }
 }
 </script>
+
+<style lang="scss">
+@import '../styles/variables';
+@import '../styles/mixins';
+
+.fv-datepicker {
+  & .header {
+    &:focus {
+      color: $primary-color;
+    }
+  }
+  & .content {
+    padding: $padding-small;
+    &:focus {
+      & .days-table td{
+        &.highlighted {
+          background: yiq($bg-color, 3%);
+          &.selected {
+            background: yiq($primary-color, 3%);
+          }
+        }
+      }
+    }
+  }
+
+  & .header-buttons{
+    overflow: visible;
+  }
+
+  & .days-table {
+    width: 100%;
+    text-align: center;
+    & td {
+      cursor: pointer;
+      
+      &,
+      th {
+        width: 14.2%;
+        height: 40px;
+        vertical-align: middle;
+        border-radius: $border-radius;
+      }
+
+      &[disabled] {
+        @include disabled;
+      }
+      
+      &:not([disabled]) {
+        &:hover {
+          background: yiq($bg-color, 3%);
+        }
+        
+        &:active {
+          background: yiq($bg-color, 5%);
+        }
+
+        &.selected {
+          @include yiq($primary-color);
+
+          &:hover {
+            background: yiq($primary-color, 3%);
+          }
+
+          &:active {
+            background: yiq($primary-color, 5%);
+          }
+        } 
+      }
+    }
+  }
+}
+</style>
