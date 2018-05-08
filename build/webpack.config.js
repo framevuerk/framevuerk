f100c73f32ce6debe1d34e35ca91d4c0c8f9f0ca
nainemom <nainemom@gmail.com>
2018-05-09 02:57:25 +0430

    refactor webpack build

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,50 +1,42 @@
-const fs = require('fs')
 const path = require('path')
 const utils = require(path.resolve(__dirname, './utils.js'))
 const pkg = require(path.resolve(__dirname, '../package.json'))
-const config = require(path.resolve(__dirname, '../.framevuerkrc.json'))
-const userConfigs = [
-  { type: 'file', path: path.resolve(__dirname, '../../../.framevuerkrc') },
-  { type: 'js', path: path.resolve(__dirname, '../../../.framevuerkrc.js') },
-  { type: 'json', path: path.resolve(__dirname, '../../../.framevuerkrc.json') },
-  { type: 'pkg', path: path.resolve(__dirname, '../../../package.json') }
-]
-const ret = []
-push(config)
-userConfigs.forEach(userConfig => {
-  if (fs.existsSync(userConfig.path)) {
-    let userConfigData = {}
-    switch (userConfig.type) {
-      case 'file':
-        userConfigData = JSON.parse(fs.readFileSync(userConfig.path))
-        break
-      case 'js':
-      case 'json':
-        userConfigData = require(userConfig.path)
-        break
-      case 'pkg':
-        const userPkg = require(userConfig.path)
-        if (userPkg.framevuerk) {
-          userConfigData = userPkg.framevuerk
-        }
-    }
-    userConfigData = Array.isArray(userConfigData) ? userConfigData : [userConfigData]
-    userConfigData.forEach(cnf => {
-      push(Object.assign(config, cnf))
-    })
-  }
-})
-
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
-function push (conf) {
-  const cnf = Object.assign(conf, {
+const configs = (() => {
+  const cosmiconfig = require('cosmiconfig')
+  const explorer = cosmiconfig(pkg.name)
+  const searchedFor = explorer.searchSync()
+  const defaultConfig = {
+    'locale': 'en',
+    'direction': 'ltr',
+    'primary-color': '#41b883',
+    'secondary-color': '#35485d',
+    'danger-color': '#dd4b39',
+    'warning-color': '#ef8f00',
+    'info-color': '#14b0cf',
+    'bg-color': '#f9f9f9',
+    'padding': '15px',
+    'transition-speed': '0.35s',
+    'border-radius': '4px',
+    'shadow-size': '3px'
+  }
+  if (searchedFor) {
+    let configs = Array.isArray(searchedFor.config) ? searchedFor.config : [searchedFor.config]
+    configs = configs.map(config => Object.assign(defaultConfig, config))
+    return configs
+  }
+  return [defaultConfig]
+})()
+
+const ret = []
+configs.forEach(config => {
+  const cnf = Object.assign(config, {
     name: pkg.name,
     version: pkg.version,
     entry: utils.resolve('../src/index.js'),
     path: utils.resolve('../dist')
   })
-
   ret.push(utils.generateConfig(Object.assign(cnf, {
     minify: false
   })))
@@ -53,6 +45,6 @@ function push (conf) {
       minify: true
     })))
   }
-}
+})
 
 module.exports = ret
