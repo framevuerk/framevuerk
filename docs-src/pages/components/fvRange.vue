ab92197bb9a62dcf432b26f63729bc2e74b57f96
nainemom <nainemom@gmail.com>
2018-05-20 19:49:28 +0430

    create fvRange docs

---
+++ docs-src/pages/components/fvRange.vue
@@ -0,0 +1,106 @@
+<template lang="pug">
+fv-content
+  div(:class="$root.mainClass")
+    doc-description
+      | To create switch in your application, use this! Note that before using this, you can add
+      =" "
+      a.fv-link(href="http://hammerjs.github.io/", target="_blank") HammerJs
+      |  as dependency of framevuerk to allow touch actions.
+    doc-code(title="Javascript:", lang="javascript")
+      = "import Framevuerk from 'framevuerk'\n"
+      = "import Hammer from 'hammerjs'\n"
+      = "Framevuerk.use('hammer', Hammer)\n"
+    doc-code
+      = "<fv-range></fv-range>"
+    doc-example
+      .fv-row
+        .fv-col-12
+          p Normal {{inputs.d1}}
+          fv-range(v-model="inputs.d1")
+        .fv-col-12
+          p Normal {{inputs.d2}}
+          fv-range(v-model="inputs.d2", multiple)
+        .fv-col-12
+          p Normal {{inputs.d3}}
+          fv-range(v-model="inputs.d3", :data="[3,6,8,13]")
+
+        //- .fv-col-sm-3
+        //-   p Invalid
+        //-   //fv-range.focus(invalid)
+        //- .fv-col-sm-3
+        //-   p Disabled
+        //-   //fv-range.fv-block(disabled)
+    doc-api(:rows="api")
+</template>
+
+<script>
+import docApi from '../../components/docApi.vue'
+import docDescription from '../../components/docDescription.vue'
+import docExample from '../../components/docExample.vue'
+import docCode from '../../components/docCode.vue'
+
+export default {
+  components: {
+    docApi,
+    docDescription,
+    docExample,
+    docCode
+  },
+  data () {
+    return {
+      inputs: {
+        d1: 1,
+        d2: [1, 4],
+        d3: 3
+      },
+      api: {
+        prop: [
+          {
+            name: 'value',
+            type: '',
+            default: '',
+            description: 'Value of input'
+          },
+          {
+            name: 'offValue',
+            type: '',
+            default: 'false',
+            description: 'Value of switch, when it is unchecked'
+          },
+          {
+            name: 'onValue',
+            type: '',
+            default: 'true',
+            description: 'Value of switch, when it is checked'
+          },
+          {
+            name: 'required',
+            type: '[Boolean, Function]',
+            default: 'false',
+            description: 'Just like normal input required attribute. Also you can pass a function to check validity of value as you want.'
+          },
+          {
+            name: 'disabled',
+            type: 'Boolean',
+            default: 'false',
+            description: 'Just like normal input disabled attribute'
+          }
+        ],
+        event: [],
+        method: [
+          {
+            name: 'toggle',
+            params: '',
+            description: 'Toggle value of switch'
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    click(){
+      this.inputs.d1 = parseInt(prompt())
+    }
+  }
+}
+</script>
