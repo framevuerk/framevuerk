fcdc2d828b79d51c506460a1dbe6710d7d7459d9
nainemom <nainemom@gmail.com>
2018-06-27 00:07:09 +0430

    remove height prop

--- src/components/fvHeader.vue
+++ src/components/fvHeader.vue
@@ -1,16 +1,11 @@
 <template lang="pug">
-header.fv-header(:style="{ height: height, maxHeight: height, minHeight: height }")
-  slot
+.fv-header
+  header
+    slot
 </template>
 
 <script>
 export default {
-  props: {
-    height: {
-      type: String,
-      default: '65px'
-    }
-  }
 }
 </script>
 
@@ -21,48 +16,79 @@ export default {
 .fv-header {
   @include shadow(bottom);
 
-  align-items: center;
-  display: flex;
-  justify-content: space-between;
-  overflow: hidden;
-  border-bottom: solid 1px $shadow-color;
-  right: 0;
-  top: 0;
-  transition-duration: $transition-speed;
-  transition-property: height;
-  width: 100%;
-  padding: 0 $padding;
   z-index: 2;
 
-  & > .fv-button,
-  & > .fv-title,
-  & > .title,
-  & > * {
-    @include nowrap;
-
+  & > header {
+    width: 100%;
+    padding: $padding;
     align-items: center;
     display: flex;
-    height: auto;
-    justify-content: center;
-    max-height: 100%;
-    max-width: 100%;
-  }
+    justify-content: space-between;
+    overflow: hidden;
 
-  & > .fv-title,
-  & > .title {
-    flex-direction: column;
-    flex-grow: 1;
-    line-height: 1.3;
-
+    & > .fv-button,
+    & > .fv-title,
+    & > .title,
     & > * {
-      margin: 0;
-      padding: 0;
-      width: 100%;
+      @include nowrap;
+
+      align-items: center;
+      display: flex;
+      height: auto;
+      justify-content: center;
+      max-height: 100%;
+      max-width: 100%;
+    }
+
+    & > .fv-title,
+    & > .title {
+      flex-direction: column;
+      flex-grow: 1;
+      line-height: 1.3;
+
+      & > * {
+        margin: 0;
+        padding: 0;
+        width: 100%;
+      }
+    }
+
+    & > .space {
+      width: $padding;
     }
   }
 
-  & > .space {
-    width: $padding;
+  &,
+  &.fv-default {
+    @include yiq($bg-color);
+
+    border-bottom: solid 1px contrast($bg-color, 2, hard-dark);
+  }
+
+  &.fv-primary {
+    @include yiq($primary-color);
+
+    border-bottom: solid 1px contrast($primary-color, 2, hard-dark);
+    & .fv-button {
+      @include fvButton($primary-color, yiq($primary-color), false);
+    }
+
+    & .fv-input {
+      @include fvInput($primary-color);
+    }
+  }
+
+  &.fv-secondary {
+    @include yiq($secondary-color);
+
+    border-bottom: solid 1px contrast($secondary-color, 2, hard-dark);
+    & .fv-button {
+      @include fvButton($secondary-color, yiq($secondary-color), false);
+    }
+
+    & .fv-input {
+      @include fvInput($secondary-color);
+    }
   }
 }
 </style>
