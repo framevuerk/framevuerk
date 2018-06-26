60f98c9c63afd3dc19bf8423b33343e5e3959800
nainemom <nainemom@gmail.com>
2018-06-27 00:01:46 +0430

    change style

--- src/components/fvButton.vue
+++ src/components/fvButton.vue
@@ -1,22 +1,30 @@
 <template lang="pug">
 button.fv-button(:class="{'loading': loading}", @click="onClick", :disabled="disabled || loading")
-  i.icon(:class="icon", v-if="icon && !loading")
-  =" "
-  span.text(v-if="!loading")
-    | {{text}}
-    slot
-  i.icon.loading-icon.fa.fa-spin.fa-circle-o-notch.fv-fast-animation(v-else)
+  template(v-if="!loading")
+    template(v-if="!text && !icon")
+      slot
+    template(v-else)
+      i.icon(v-if="icon", :class="icon")
+      =" "
+      span.text(v-if="text", v-text="text")
+      slot
+  fv-loading(v-else)
 </template>
 
 <script>
+import fvLoading from './fvLoading.vue'
+
 export default {
+  components: {
+    fvLoading
+  },
   props: {
     loading: {
       type: Boolean,
       default: false
     },
     icon: {
-      default: ''
+      default: undefined
     },
     text: {
       default: undefined
@@ -41,41 +49,7 @@ export default {
 @import '../styles/functions';
 @import '../styles/mixins';
 
-@mixin button($clr, $outline: false, $active-color: false, $invert: false) {
-  $outline-color: if($outline, $outline, $clr);
-  $background: if($invert, $clr, $bg-color);
-  $active-color: if($invert, yiq($background, 10%), if($active-color, $active-color, $outline-color));
-  $color: if($invert, yiq($clr), $clr);
-
-  background-color: $background;
-  color: $color;
-  border: 1px solid darken($bg-color-light, $shadow-percent);
-
-  &:hover {
-    background-color: yiq($background, 2%);
-  }
-
-  &:focus {
-    background-color: yiq($background, 2%);
-
-    &:invalid,
-    &[invalid] {
-      @include outline($danger-color);
-    }
-  }
-
-  &:active:not([disabled]) {
-    background-color: $active-color;
-    color: yiq($active-color);
-  }
-
-  &[disabled] {
-    @include disabled;
-  }
-}
-
 .fv-button {
-  @include shadow(bottom);
   @include sizes;
 
   border-radius: $border-radius;
@@ -87,7 +61,8 @@ export default {
   font-weight: bold;
   padding: 0 $padding;
   transition-duration: $transition-speed-fast;
-  transition-property: background-color, color;
+  transition-property: background-color, color, box-shadow;
+  border: none;
 
   &,
   & .text,
@@ -95,52 +70,37 @@ export default {
     @include nowrap;
 
     vertical-align: middle;
+
+    & > svg {
+      vertical-align: middle;
+      height: 1.7em;
+    }
   }
 
   &,
   &.fv-default {
-    @include button(yiq($bg-color), $primary-color, yiq($bg-color, 10%));
+    @include fvButton($bg-color, yiq($bg-color));
   }
 
   &.fv-primary,
   &.fv-ok {
-    @include button($primary-color);
-
-    &.invert {
-      @include button($primary-color, false, false, true);
-    }
+    @include fvButton($primary-color, yiq($primary-color));
   }
 
   &.fv-secondary {
-    @include button($secondary-color);
-
-    &.invert {
-      @include button($secondary-color, false, false, true);
-    }
+    @include fvButton($secondary-color, yiq($secondary-color));
   }
 
   &.fv-info {
-    @include button($info-color);
-
-    &.invert {
-      @include button($info-color, false, false, true);
-    }
+    @include fvButton($info-color, yiq($info-color));
   }
 
   &.fv-danger {
-    @include button($danger-color);
-
-    &.invert {
-      @include button($danger-color, false, false, true);
-    }
+    @include fvButton($danger-color, yiq($danger-color));
   }
 
   &.fv-warning {
-    @include button($warning-color);
-
-    &.invert {
-      @include button($warning-color, false, false, true);
-    }
+    @include fvButton($warning-color, yiq($warning-color));
   }
 }
 </style>
