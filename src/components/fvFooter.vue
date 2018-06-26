2b6beab8d011d12594a7c2c813549cda98070e8c
nainemom <nainemom@gmail.com>
2018-06-27 00:11:16 +0430

    remove height prop

--- src/components/fvFooter.vue
+++ src/components/fvFooter.vue
@@ -1,16 +1,11 @@
 <template lang="pug">
-footer.fv-footer(:style="{ height: height, maxHeight: height, minHeight: height }")
-  slot
+.fv-footer
+  footer
+    slot
 </template>
 
 <script>
 export default {
-  props: {
-    height: {
-      type: String,
-      default: '65px'
-    }
-  }
 }
 </script>
 
@@ -19,32 +14,35 @@ export default {
 @import '../styles/mixins';
 
 .fv-footer {
+  // $footer-bg-color: #fff;
+
+  @include yiq($footer-bg-color);
   @include shadow(inset-bottom);
 
-  align-items: center;
-  bottom: 0;
-  border-top: solid 1px $shadow-color;
-  display: flex;
-  justify-content: space-between;
-  overflow: hidden;
-  padding: $padding-small $padding;
-  width: 100%;
+  & > footer {
+    align-items: center;
+    border-top: solid 1px contrast($footer-bg-color, 2, hard-dark);
+    display: flex;
+    justify-content: space-between;
+    overflow: hidden;
+    padding: $padding-small $padding;
 
-  & .fv-title,
-  & .title {
-    padding: 0 $padding;
-    text-align: center;
+    & .fv-title,
+    & .title {
+      padding: 0 $padding;
+      text-align: center;
 
-    & > * {
-      @include nowrap;
+      & > * {
+        @include nowrap;
 
-      margin: 0;
-      width: 100%;
+        margin: 0;
+        width: 100%;
+      }
     }
-  }
 
-  & > *:not(:last-child) {
-    margin-#{$block-end}: $padding;
+    & > *:not(:last-child) {
+      margin-#{$block-end}: $padding;
+    }
   }
 }
 </style>
