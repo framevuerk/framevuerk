f2f958de0220a837e8c23f39f43e3562c602ec50
nainemom <nainemom@gmail.com>
2018-07-10 02:16:04 +0430

    update style

--- src/components/fvCheck.vue
+++ src/components/fvCheck.vue
@@ -149,7 +149,9 @@ export default {
       border: none;
       background: $primary-color;
       position: absolute;
-      transform: translate(50%, 50%);
+      transform: translate(-50%, -50%);
+      left: 50%;
+      top: 50%;
     }
 
     &[multiple] > .box::before {
@@ -162,7 +164,9 @@ export default {
       border-top: none;
       border-right: none;
       position: absolute;
-      transform: translate(7%, 35%) rotateZ(-45deg);
+      transform: rotateZ(-45deg);
+      left: 5%;
+      top: 21%;
     }
   }
 
