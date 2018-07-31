ea80c306b4a9bc1038e08dfd08066e29fb1c4e2f
nainemom <nainemom@gmail.com>
2018-07-31 18:08:13 +0430

    add lock functionality

--- src/components/fvMain.vue
+++ src/components/fvMain.vue
@@ -1,8 +1,6 @@
 <template lang="pug">
-.fv-main
+.fv-main(:style="{paddingLeft: offset.left, paddingRight: offset.right}")
   slot
-  transition(name="fv-fade")
-    .fv-overlay(v-if="isLocked", @click="$emit('overlay-click', $event)")
 </template>
 
 <script>
@@ -15,15 +13,73 @@ export default {
   },
   data () {
     return {
-      isLocked: false
+      locks: 0,
+      offset: {
+        left: 0,
+        right: 0
+      }
     }
   },
   methods: {
-    lock () {
-      this.isLocked = true
+    $appendChild (child) {
+      this.$el.appendChild(child)
     },
-    unlock () {
-      this.isLocked = false
+    $lock (fn = () => {}, manualClose = false) {
+      const hash = `${window.location.hash.indexOf('?') !== -1 ? '&' : '?'}fv${new Date().valueOf()}`
+      let hashChange = true // some hash changes applied by this component and does not need to be handeled. this variable keep state of this.
+      const unlock = () => {
+        window.removeEventListener('hashchange', onHashChange)
+        removeHash()
+        if (overlay) {
+          overlay.remove()
+        }
+        if (typeof fn === 'function') {
+          fn()
+        }
+      }
+      const addHash = () => {
+        if (window.location.hash.indexOf(hash) === -1) {
+          window.location.hash += hash
+        }
+      }
+      const removeHash = () => {
+        window.location.hash = window.location.hash.replace(hash, '')
+      }
+      const onHashChange = (event) => {
+        if (hashChange) {
+          addHash()
+        } else if (!manualClose) {
+          unlock()
+        }
+        hashChange = !hashChange
+      }
+
+      const overlay = document.createElement('div')
+      overlay.classList.add('fv-overlay')
+      if (!manualClose) {
+        overlay.addEventListener('click', unlock, {
+          once: true
+        })
+      }
+
+      addHash()
+      this.$appendChild(overlay)
+
+      this.$nextTick(() => {
+        setTimeout(() => {
+          window.addEventListener('hashchange', onHashChange)
+        })
+      })
+      return unlock
+    },
+    $unlock (lockId) {
+      if (typeof lockId === 'function') {
+        lockId()
+        lockId = null
+      }
+    },
+    setOffset (position = 'left', size = '0px') {
+      this.offset[position] = size
     }
   }
 }
