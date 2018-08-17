b3a4b77bf362b360187399986d4b9c4365341c03
nainemom <nainemom@gmail.com>
2018-08-18 01:25:17 +0430

    clean

--- src/components/fvMain.vue
+++ src/components/fvMain.vue
@@ -21,63 +21,6 @@ export default {
     }
   },
   methods: {
-    $appendChild (child) {
-      this.$el.appendChild(child)
-    },
-    $lock (fn = () => {}, manualClose = false) {
-      const hash = `${window.location.hash.indexOf('?') !== -1 ? '&' : '?'}fv${new Date().valueOf()}`
-      let hashChange = true // some hash changes applied by this component and does not need to be handeled. this variable keep state of this.
-      const unlock = () => {
-        window.removeEventListener('hashchange', onHashChange)
-        removeHash()
-        if (overlay) {
-          overlay.remove()
-        }
-        if (typeof fn === 'function') {
-          fn()
-        }
-      }
-      const addHash = () => {
-        if (window.location.hash.indexOf(hash) === -1) {
-          window.location.hash += hash
-        }
-      }
-      const removeHash = () => {
-        window.location.hash = window.location.hash.replace(hash, '')
-      }
-      const onHashChange = (event) => {
-        if (hashChange) {
-          addHash()
-        } else if (!manualClose) {
-          unlock()
-        }
-        hashChange = !hashChange
-      }
-
-      const overlay = document.createElement('div')
-      overlay.classList.add('fv-overlay')
-      if (!manualClose) {
-        overlay.addEventListener('click', unlock, {
-          once: true
-        })
-      }
-
-      addHash()
-      this.$appendChild(overlay)
-
-      this.$nextTick(() => {
-        setTimeout(() => {
-          window.addEventListener('hashchange', onHashChange)
-        })
-      })
-      return unlock
-    },
-    $unlock (lockId) {
-      if (typeof lockId === 'function') {
-        lockId()
-        lockId = null
-      }
-    },
     setOffset (position = 'left', size = '0px') {
       this.offset[position] = size
     }
