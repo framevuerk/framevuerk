f690d44b9566aeb0f38febcf0fc95191cefdaa3c
nainemom <nainemom@gmail.com>
2018-08-22 06:19:26 +0430

    clean-up style

--- src/components/fvFooter.vue
+++ src/components/fvFooter.vue
@@ -14,35 +14,15 @@ export default {
 @import '../styles/mixins';
 
 .fv-footer {
-  // $footer-bg-color: #fff;
-
   @include yiq($footer-bg-color);
   @include shadow(inset-bottom);
 
+  border-top: solid 1px contrast($footer-bg-color, 2, hard-dark);
+  height: auto;
+  overflow: auto;
+
   & > footer {
-    align-items: center;
-    border-top: solid 1px contrast($footer-bg-color, 2, hard-dark);
-    display: flex;
-    justify-content: space-between;
-    overflow: hidden;
-    padding: $padding-small $padding;
-
-    & .fv-title,
-    & .title {
-      padding: 0 $padding;
-      text-align: center;
-
-      & > * {
-        @include nowrap;
-
-        margin: 0;
-        width: 100%;
-      }
-    }
-
-    & > *:not(:last-child) {
-      margin-#{$block-end}: $padding;
-    }
+    padding: #{$padding/1.2} $padding;
   }
 }
 </style>
