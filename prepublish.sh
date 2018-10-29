db91e61e0f5b9bfff26ff59961737af1ecc2bd9c
nainemom <nainemom@gmail.com>
2018-10-29 18:19:32 +0330

    update prepublish commands

--- prepublish.sh
+++ prepublish.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
-
+npm run clean
+rm -rf ./node_modules
 npm i
 npm rebuild node-sass
-npm run clean
 npm run build
-
-git clone https://github.com/framevuerk/public.git
-
-
-
-cd ./public
-git rm -r *
-cp -r ../docs/* ./
-git add --all
-git commit -m 'updated'
-git push -f origin master
-
-cd ..
-rm -rf ./public
