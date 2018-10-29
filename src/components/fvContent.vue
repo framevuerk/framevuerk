8e28bef64cdec6a76e68ef985ecb1c5428bfff2a
nainemom <nainemom@gmail.com>
2018-10-29 09:39:12 +0330

    add initialScroll prop

--- src/components/fvContent.vue
+++ src/components/fvContent.vue
@@ -3,6 +3,33 @@
   slot
 </template>
 
+<script>
+export default {
+  props: {
+    initialScroll: {
+      type: Number,
+      default: 0
+    }
+  },
+  mounted () {
+    this.$el.style.opacity = 0
+    setTimeout(() => {
+      if (this.initialScroll) {
+        this.$el.style.opacity = 0
+        this.$nextTick(() => {
+          setTimeout(() => {
+            this.$el.scrollTop = this.initialScroll
+            this.$el.style.opacity = 1
+          })
+        })
+      } else {
+        this.$el.style.opacity = 1
+      }
+    })
+  }
+}
+</script>
+
 <style lang="scss">
 @import '../styles/variables';
 @import '../styles/mixins';
@@ -12,8 +39,11 @@
 
   display: block;
   flex-grow: 1;
-  overflow: auto;
+  overflow-x: hidden;
+  overflow-y: auto;
   width: 100%;
+  max-width: 100%;
+  transition: opacity 0.1s;
 
   &::after {
     clear: both;
