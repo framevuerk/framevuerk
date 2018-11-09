5e6990e884d7886bbe4deb5e5251dd41e9ca4e7e
Nainemom <nainemom@gmail.com>
2018-11-10 03:10:59 +0330

    remove sourceElement prop and calculate position of clicked point in new way

--- src/components/fvMenu.vue
+++ src/components/fvMenu.vue
@@ -1,96 +1,132 @@
 <template lang="pug">
-fv-dialog.fv-menu(ref="dialog",
-  :style="dialogStyle",
-  :class="dialogClass",
-  :value="value",
-  @input="$emit('input', $event)",
-  @close="$emit('close')",
-  @open="onOpen")
-  fv-content
-    fv-list(ref="list",
-      parent)
-      fv-list-item(v-for="item in items",
-        :key="itemProp(item, 'value')",
-        @click="onItemClick(item)",
-        :disabled="itemProp(item, 'disabled')")
-        slot(v-if="$scopedSlots.default || $slots.default", :item="item")
-        span(v-else) {{itemProp(item, 'text')}}
+transition(name="fv-menu")
+  .fv-menu(v-show="value")
+    slot
 </template>
 
 <script>
-import utility from '../utility'
+import parent from '../utility/parent.js'
+import CancelDetector from '../utility/CancelDetector.js'
+import FocusStoler from '../utility/FocusStoler.js'
 
 export default {
   props: {
     value: {
       type: Boolean,
       default: false
-    },
-    sourceElement: {
-      type: HTMLElement
-    },
-    items: {
-      type: Array,
-      default: () => []
-    },
-    textKey: {
-      type: String,
-      default: 'text'
-    },
-    disabledKey: {
-      type: String,
-      default: 'disabled'
     }
   },
-  inject: ['fvMain'],
   data () {
     return {
-      dialogStyle: {},
-      dialogClass: []
+      cancelDetector: new CancelDetector(),
+      focusStoler: new FocusStoler(),
+      lastClickedPosition: null
     }
   },
   methods: {
-    onOpen () {
-      this.$emit('open')
-      const isSmall = this.fvMain.getSize().indexOf('md') === -1
-      if (isSmall) {
-        this.dialogStyle = {
-          width: `calc(100% - 30px)`,
-          maxHeight: `calc(100% - 60px)`,
-          bottom: '15px',
-          left: '15px',
-          right: '15px'
-        }
-        this.dialogClass = ['not-center']
-      } else if (this.sourceElement) {
-        const offset = utility.offsetTo(this.sourceElement, this.fvMain.$el)
-        this.dialogStyle = {
-          left: `${offset.left}px`,
-          top: `${offset.top}px`
-        }
-        this.dialogClass = ['not-center']
-      } else {
-        this.dialogStyle = {}
-        this.dialogClass = []
+    valueHandler (value) {
+      if (value) {
+        this.$emit('open')
+        parent.$el.appendChild(this.$el)
+        parent.on('outsideclick', this.$el, this.close)
+        this.cancelDetector.start(this.close)
+        this.$nextTick(() => {
+          this.setPosition()
+          this.focusStoler.stole(this.$el)
+        })
+        return
       }
+      this.$emit('close')
+      parent.off('outsideclick', this.$el, this.close)
+      this.cancelDetector.stop()
+      this.focusStoler.restore()
     },
-    itemProp (item, prop = 'text') {
-      switch (prop) {
-        case 'text':
-          return this.textKey ? item[this.textKey] : item
-        case 'disabled':
-          return this.disabledKey ? item[this.disabledKey] : false
+    setPosition () {
+      if (!this.lastClickedPosition) {
+        return
       }
+      const offset = this.lastClickedPosition
+      const viewport = parent.getViewport()
+      // a number between 0 and 1. lowest number is nearest to start direction of block
+      const horizontalPosition = offset[process.env.blockStart] / viewport.width
+      // it will be one of [0, 1, 2]. lowest number is nearest to top
+      const verticalPosition = offset.viewportTop / viewport.height
+
+      const basedOn = {
+        nx: horizontalPosition > 0.45 ? process.env.blockStart : process.env.blockEnd,
+        x: horizontalPosition > 0.45 ? process.env.blockEnd : process.env.blockStart,
+        ny: verticalPosition > 0.7 ? 'top' : 'bottom',
+        y: verticalPosition > 0.7 ? 'bottom' : 'top'
+      }
+      this.$el.style[basedOn.x] = `${offset[basedOn.x]}px`
+      this.$el.style[basedOn.ny] = 'auto'
+      this.$el.style[basedOn.nx] = 'auto'
+      this.$el.style[basedOn.y] = `${offset[basedOn.y]}px`
     },
-    onItemClick (item) {
-      this.$emit('click', item)
+    close () {
       this.$emit('input', false)
+    },
+    lastClickPositionHandler (event) {
+      console.log('lclick handler')
+      const viewport = parent.getViewport()
+      const scrollPos = parent.getScrollPosition()
+      const offset = {
+        left: event.clientX,
+        top: event.clientY + scrollPos.top,
+        viewportTop: event.clientY
+      }
+      offset.right = viewport.width - offset.left
+      offset.bottom = window.document.scrollingElement.offsetHeight - offset.top
+      this.lastClickedPosition = offset
+    }
+  },
+  watch: {
+    value (value) {
+      this.valueHandler(value)
     }
   },
   created () {
-    if (!this.fvMain) {
-      throw utility.error('no_fvmain_parent')
-    }
+    parent.on('click', this.lastClickPositionHandler, true)
+    parent.on('contextmenu', this.lastClickPositionHandler, true)
+  },
+  beforeDestroy () {
+    parent.off('click', this.lastClickPositionHandler, true)
+    parent.off('contextmenu', this.lastClickPositionHandler, true)
+    this.$el.remove()
+    this.valueHandler(false)
   }
 }
 </script>
+
+<style lang="scss">
+@import '../styles/variables';
+@import '../styles/mixins';
+
+.fv-menu {
+  @include yiq($bg-color);
+  @include shadow(bottom);
+
+  backface-visibility: hidden;
+  height: auto;
+  border: solid 1px contrast($bg-color, 2, hard-dark);
+  position: absolute;
+  border-radius: $border-radius;
+  z-index: 3;
+
+  &.fv-menu-enter-active,
+  &.fv-menu-leave-active {
+    transform: translate3d(0, 0, 0);
+    transition-property: transform, opacity;
+    transition-timing-function: ease;
+    transition-duration: $transition-speed-fast;
+    will-change: transform, opacity;
+    backface-visibility: hidden;
+  }
+
+  &.fv-menu-enter,
+  &.fv-menu-leave-active {
+    opacity: 0;
+    transform: translate3d(0, -1em, 0);
+  }
+}
+</style>
