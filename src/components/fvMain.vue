fe03a5efc3aa4d2249ef87a6225856a1f0da1984
Nainemom <nainemom@gmail.com>
2018-11-10 03:10:17 +0330

    delete parent prop and functionality

--- src/components/fvMain.vue
+++ src/components/fvMain.vue
@@ -1,64 +1,27 @@
 <template lang="pug">
-.fv-main(:style="{paddingLeft: offset.left, paddingRight: offset.right}", :data-parent="parent")
+.fv-main
   slot
 </template>
 
-<script>
-import utility from '../utility/index.js'
-
-export default {
-  props: {
-    parent: {
-      type: Boolean,
-      default: false
-    }
-  },
-  data () {
-    return {
-      offset: {
-        left: 0,
-        right: 0
-      }
-    }
-  },
-  methods: {
-    getSize () {
-      return utility.viewportSize(this.$el)
-    },
-    appendChild (el) {
-      this.$el.appendChild(el)
-    },
-    appendOverlay (beforeEl, onClick) {
-      const overlayElement = document.createElement('div')
-      overlayElement.classList.add('fv-overlay')
-      overlayElement.addEventListener('click', onClick)
-      this.$el.insertBefore(overlayElement, beforeEl)
-      return overlayElement
-    }
-  },
-  created () {
-    if (this.parent) {
-      this._provided = {
-        fvMain: this
-      }
-    }
-  }
-}
-</script>
-
 <style lang="scss">
+// style packages that affect to output
+@import '../styles/reset';
+@import '../styles/global';
+@import '../styles/grid';
+@import '../styles/sizes';
+@import '../styles/helper';
+// helper packages used in .fv-main css props
 @import '../styles/variables';
 @import '../styles/mixins';
 
 .fv-main {
   @include yiq($bg-color);
 
-  direction: $direction;
   display: flex;
-  flex-direction: column;
-  height: inherit;
-  max-height: 100%;
-  overflow: hidden;
-  position: relative;
+  flex-direction: row;
+
+  &.column {
+    flex-direction: column;
+  }
 }
 </style>
