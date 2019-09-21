f47a8433c517d69a59d28fb76ce2923b6db975c8
nainemom <nainemom@gmail.com>
2019-09-21 19:06:31 +0430

    create fvSlide component

---
+++ src/components/fvSlide.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+.fv-slide
+  slot
+</template>
+
+<script>
+import icon from '../icons/ARR.svg'
+import parent from '../utility/parent.js'
+
+export default {
+  props: {
+    value: {
+      required: true
+    }
+  },
+  inject: {
+    fvSlider: {
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../styles/variables';
+@import '../styles/mixins';
+
+.fv-slide {
+  padding: 0;
+  overflow-x: hidden;
+  display: inline-block;
+  vertical-align: top;
+  user-select: none;
+  margin: 0;
+}
+
+</style>
