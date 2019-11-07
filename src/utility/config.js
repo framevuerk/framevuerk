83f7f66a39356683f23501fd00c1869bbb7fb58e
Amir Momenian <nainemom@gmail.com>
2019-11-08 01:27:35 +0330

    use HSL instead of RGB

--- src/utility/config.js
+++ src/utility/config.js
@@ -2,59 +2,105 @@ import parent from './parent.js'
 
 class Config {
   constructor () {
-    this.config = {}
+    this.value = []
   }
-  hexToRgb (hex) {
-    // https://stackoverflow.com/a/11508164
-    const bigint = (() => {
-      let pHex = hex
-      if (pHex.indexOf('#') === 0) {
-        pHex = pHex.substr(1)
+  toHsl (hexString) {
+    // https://gist.github.com/mjackson/5311256
+    let hex = hexString.replace('#', '')
+    if (hex.length === 3) {
+      hex = hex[0].repeat(2) + hex[1].repeat(2) + hex[2].repeat(2)
+    }
+    if (hex.length !== 6) {
+      return [0, 0, 100]
+    }
+    hex = hex.match(/.{1,2}/g)
+    let [red, green, blue] = hex.map(part => parseInt(part, 16) / 255)
+    const min = Math.min(red, green, blue)
+    const max = Math.max(red, green, blue)
+    const offset = max - min
+    const sum = max + min
+    let hue = 0
+    let saturation = 0
+    let luminace = Math.floor((sum / 2) * 100)
+    if (min !== max) {
+      saturation = Math.floor(luminace > 50 ? ((offset / (2 - offset)) * 100) : ((offset / sum) * 100))
+      switch (max) {
+        case red:
+          hue = (green - blue) / offset + (green < blue ? 6 : 0)
+          break
+        case green:
+          hue = (blue - red) / offset + 2
+          break
+        case blue:
+          hue = (red - green) / offset + 4
+          break
       }
-      if (pHex.length === 3) {
-        pHex = pHex[0] + pHex[0] + pHex[1] + pHex[1] + pHex[2] + pHex[2]
-      }
-      if (pHex.length !== 6) {
-        pHex = 'ffffff'
-      }
-      return parseInt(pHex, 16)
-    })()
+      hue = Math.floor(hue * 60)
+    }
+    return [hue, saturation, luminace]
+  }
+  lighten (color, percetange) {
     return [
-      (bigint >> 16) & 255,
-      (bigint >> 8) & 255,
-      bigint & 255
+      color[0],
+      color[1],
+      Math.max(0, Math.min(color[2] + percetange, 100))
     ]
   }
-  pushColor (color, amount) {
-    return color.map(p => {
-      return parseInt(p * (100 + amount) / 100)
-    })
+  isDark (color) {
+    return color[2] < 50
   }
-  luma (color) {
-    return 0.299 * color[0] + 0.587 * color[1] + 0.114 * color[2]
+  yiq (color, percetange) {
+    return this.lighten(color, color[2] < 60 ? percetange : -1 * percetange)
   }
-  yiq (color, percetange = 90, threshold = 180) {
-    let amount = percetange
-    if (this.luma(color) > threshold) {
-      amount *= -1
+  nearColors (color) {
+    const isTooLight = color[2] > 80
+    const isTooDark = color[2] < 20
+    // const hsl = this.toHsl(color)
+    return {
+      hover: this.lighten(color, isTooLight ? -6 : 6),
+      active: this.lighten(color, isTooDark ? 12 : -12),
+      border: this.lighten(color, isTooDark ? 10 : -10),
+      borderhover: this.lighten(color, isTooDark ? 30 : -30),
+      light: this.lighten(color, 8),
+      dark: this.lighten(color, -8),
+      placeholder: this.yiq(color, 30),
+      text: this.yiq(color, 90),
+      normal: color
     }
-    return this.pushColor(color, amount)
   }
-  injectOrReplaceStyleTag (content, id = 'fv-config') {
+  applyConfig (config) {
+    this.value = config
+    let cssContent = ':root{'
+    this.value.forEach(data => {
+      if (data.type === 'color') {
+        Object.keys(data.value).forEach(amt => {
+          cssContent += `--${data.type}-${data.name}-${amt}: hsl(${data.value[amt][0]},${data.value[amt][1]}%,${data.value[amt][2]}%);`
+        })
+      } else if (data.type === 'size' || data.type === 'speed') {
+        Object.keys(data.value).forEach(amt => {
+          cssContent += `--${data.type}-${data.name}-${amt}: ${data.value[amt]}${data.unit};`
+        })
+      } else if (data.type === 'direction') {
+        cssContent += `--direction: ${data.value.direction};`
+        cssContent += `--block-start: ${data.value.blockStart};`
+        cssContent += `--block-end: ${data.value.blockEnd};`
+      }
+    })
+    cssContent += '}'
     return new Promise(resolve => {
       const tryInject = () => {
         if (!parent.$head) {
           parent.calcWindow()
           setTimeout(tryInject, 100)
         }
-        const oldStyleElem = parent.$document.getElementById(id)
+        const oldStyleElem = parent.$document.getElementById('fv-config')
         if (oldStyleElem) {
-          oldStyleElem.innerHTML = content
+          oldStyleElem.innerHTML = cssContent
         } else {
           const styleElem = parent.$document.createElement('STYLE')
           styleElem.type = 'text/css'
-          styleElem.innerHTML = content
-          styleElem.id = id
+          styleElem.innerHTML = cssContent
+          styleElem.id = 'fv-config'
           console.warn(styleElem)
           parent.$head.appendChild(styleElem)
         }
@@ -64,53 +110,126 @@ class Config {
       tryInject()
     })
   }
-  applyConfig (value = {}) {
-    const config = {
-      colors: {},
-      sizes: {}
-    }
-    let styleContent = ''
-    Object.keys(value.colors || {}).forEach(color => {
-      const rgb = this.hexToRgb(value.colors[color])
-      console.log(rgb, value.colors[color])
-      const amountNames = ['darker', 'dark', 'normal', 'light', 'lighter'];
-      [-20, -10, 0, 10, 20].forEach((amount, index) => {
-        const amountName = amountNames[index]
-        const value = this.pushColor(rgb, amount)
-        const yiqValue = this.yiq(rgb)
-        config.colors[`${color}-${amountName}`] = value
-        config.colors[`${color}-${amountName}-yiq`] = yiqValue
-        styleContent += `--colors-${color}-${amountName}: rgb(${value.join(',')});`
-        styleContent += `--colors-${color}-${amountName}-yiq: rgb(${yiqValue.join(',')});`
-      // config.colors[`${color}-${amountName}`] = this.pushColor(rgb, amount)
-      // config.colors[`${color}-${amountName}`] = `rgb(${this.pushColor(rgb, amount).join(',')})`
-      // config.colors[`${color}-${amountName}-yiq`] = `rgb(${this.yiq(rgb).join(',')})`
-      // ret += `--color-${color}-${amountName}: rgb(${this.pushColor(rgb, amount).join(',')});`
-      // ret += `--color-${color}-${amountName}-yiq: rgb(${this.yiq(rgb).join(',')});`
-      })
+  setConfig (userConfig = []) {
+    const defaultConfig = [
+      {
+        name: 'main',
+        type: 'direction',
+        value: 'left'
+      },
+      {
+        name: 'padding',
+        type: 'size',
+        value: '1em'
+      },
+      {
+        name: 'border-radius',
+        type: 'size',
+        value: '4px'
+      },
+      {
+        name: 'shadow',
+        type: 'size',
+        value: '4px'
+      },
+      {
+        name: 'transition',
+        type: 'speed',
+        value: '0.35s'
+      },
+      {
+        name: 'background',
+        type: 'color',
+        value: '#ffffff'
+      },
+      {
+        name: 'primary',
+        type: 'color',
+        value: '#41b883'
+      },
+      {
+        name: 'secondary',
+        type: 'color',
+        value: '#35485d'
+      },
+      {
+        name: 'info',
+        type: 'color',
+        value: '#14b0cf'
+      },
+      {
+        name: 'warning',
+        type: 'color',
+        value: '#ef8f00'
+      },
+      {
+        name: 'danger',
+        type: 'color',
+        value: '#dd4b39'
+      },
+      {
+        name: 'header',
+        type: 'color',
+        value: '#35485d'
+      },
+      {
+        name: 'sidebar',
+        type: 'color',
+        value: '#3a3a3a'
+      },
+      {
+        name: 'footer',
+        type: 'color',
+        value: '#3a3a3a'
+      },
+    ]
+    const config = JSON.parse(JSON.stringify(userConfig))
+    const ret = []
+    defaultConfig.concat(config).forEach(item => {
+      if (item.type === 'color') {
+        const value = this.toHsl(item.value)
+        console.log(item.name, item.value, value, this.nearColors(value))
+        ret.push({
+          name: item.name,
+          type: 'color',
+          value: this.nearColors(value)
+        })
+      } else if (item.type === 'size') {
+        const value = parseFloat(item.value)
+        ret.push({
+          name: item.name,
+          type: 'size',
+          unit: item.value.toString().replace(/\d+/g, '').replace('.', '') || 'em',
+          value: {
+            small: value / 2,
+            normal: value,
+            large: value * 1.5
+          }
+        })
+      } else if (item.type === 'direction') {
+        ret.push({
+          type: 'direction',
+          value: {
+            direction: item.value === 'left' ? 'ltr' : 'rtl',
+            blockStart: item.value === 'left' ? 'left' : 'right',
+            blockEnd: item.value === 'left' ? 'right' : 'left'
+          }
+        })
+      } else if (item.type === 'speed') {
+        const value = parseFloat(item.value)
+        ret.push({
+          name: item.name,
+          type: 'speed',
+          unit: item.value.toString().replace(/\d+/g, '').replace('.', '') || 'em',
+          value: {
+            fast: value / 2,
+            normal: value,
+            slow: value * 2
+          }
+        })
+      }
     })
-    Object.keys(value.sizes || {}).forEach(size => {
-      const num = Number(value.sizes[size])
-      const ext = size.toString().replace(/\d+/g, '') || 'em'
-      const amountNames = ['sm', 'md', 'lg'];
-      [0.5, 0, 1.5].forEach((amount, index) => {
-        const amountName = amountNames[index]
-        const value = num * amount
-        config.sizes[`${size}-${amountName}`] = [value, ext]
-        styleContent += `--sizes-${size}-${amountName}: ${value}${ext};`
-      })
-      // const smValue = [num / 2, ext]
-      // const mdValue = [num, ext]
-      // const lgValue = [num * 1.5, ext]
-      // config.sizes[`${size}-sm`] = smValue
-      // ret += `--size-${size}-small: ${num / 2}${ext};`
-      // ret += `--size-${size}-normal: ${num}${ext};`
-      // ret += `--size-${size}-large: ${num * 1.5}${ext};`
-    })
-    if (JSON.stringify(config) !== JSON.stringify(this.config)) {
-      this.config = config
-      this.injectOrReplaceStyleTag(`:root{${styleContent}--direction: border-left;}`)
-    }
+    this.applyConfig(ret)
   }
 }
 
