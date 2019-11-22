d98d4c85172ecad3a4ba4db1a3dc51cb58d1c1b5
Amir Momenian <nainemom@gmail.com>
2019-11-23 00:36:17 +0330

    create formElement mixin

---
+++ src/mixins/formElement.js
@@ -0,0 +1,48 @@
+export default function (fillingCheck = () => true) {
+  return {
+    props: {
+      value: {
+        default: undefined
+      },
+      disabled: {
+        type: Boolean,
+        default: false
+      },
+      required: {
+        type: [Boolean, Function],
+        default: false
+      }
+    },
+    inject: {
+      fvFormElement: {
+        default: false
+      }
+    },
+    computed: {
+      fvValidate () {
+        if (this.required === true) {
+          return fillingCheck(this.value)
+        }
+        if (typeof this.required === 'function') {
+          return this.required(this.value)
+        }
+        return true
+      }
+    },
+    methods: {
+      focus () {
+        this.$el.focus()
+      },
+      onFocusDefault () {
+        if (this.fvFormElement) {
+          this.fvFormElement.turn(true)
+        }
+      },
+      onBlurDefault () {
+        if (this.fvFormElement) {
+          this.fvFormElement.turn(false)
+        }
+      }
+    }
+  }
+}
