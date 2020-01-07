0633381ebd06abe188e7b9e31d4715db974bcd35
Amir Momenian <nainemom@gmail.com>
2020-01-07 23:04:11 +0330

    update sidebar

--- docs-src/components/appSidebar.vue
+++ docs-src/components/appSidebar.vue
@@ -10,62 +10,12 @@
       </div>
       <fvList v-if="typeof searchResult === 'undefined'">
         <fvListItem expanded class="unclickable">
-            <i class="fa fa-code" /> Installation
+            <i class="fa fa-code" /> Components
             <fvList slot="sub-list">
-              <fvListItem tag="router-link" :to="item.route" v-for="item in sidebarItems.installationItems" :key="item.text" :class="{ selected: $route.path === item.route }">
-                  {{item.text}}
+              <fvListItem tag="router-link" to="/components/footer" :class="{ selected: $route.params.component === 'footer' }">
+                Footer
               </fvListItem>
             </fvList>
-        </fvListItem>
-        <fvListItem expanded class="unclickable">
-            <i class="fa fa-cubes" /> Components
-            <fvList slot="sub-list">
-              <fvListItem expanded class="unclickable">
-                  <i class="fa fa-pencil-square-o" /> Form
-                  <fvList slot="sub-list">
-                    <fvListItem v-for="item in sidebarItems.componentFormItems" :key="item.text" :class="{ selected: $route.path === item.route }">
-                        <router-link class="fv-block" :to="item.route">{{item.text}}</router-link>
-                    </fvListItem>
-                  </fvList>
-              </fvListItem>
-              <fvListItem expanded class="unclickable">
-                  <i class="fa fa-info-circle" /> Dialog
-                  <fvList slot="sub-list">
-                    <fvListItem v-for="item in sidebarItems.componentDialogItems" :key="item.text" :class="{ selected: $route.path === item.route }">
-                        <router-link class="fv-block" :to="item.route">{{item.text}}</router-link>
-                    </fvListItem>
-                  </fvList>
-              </fvListItem>
-              <fvListItem expanded class="unclickable">
-                  <i class="fa fa-th-large" /> Layout
-                  <fvList slot="sub-list">
-                    <fvListItem v-for="item in sidebarItems.componentLayoutItems" :key="item.text" :class="{ selected: $route.path === item.route }">
-                        <router-link class="fv-block" :to="item.route">{{item.text}}</router-link>
-                    </fvListItem>
-                  </fvList>
-              </fvListItem>
-              <fvListItem expanded class="unclickable">
-                  <i class="fa fa-ellipsis-h" /> Other
-                  <fvList slot="sub-list">
-                    <fvListItem v-for="item in sidebarItems.componentOtherItems" :key="item.text" :class="{ selected: $route.path === item.route }">
-                        <router-link class="fv-block" :to="item.route">{{item.text}}</router-link>
-                    </fvListItem>
-                  </fvList>
-              </fvListItem>
-            </fvList>
-        </fvListItem>
-        <fvListItem expanded class="unclickable">
-            <i class="fa fa-css3" /> Styles
-            <fvList slot="sub-list">
-              <fvListItem v-for="item in sidebarItems.styleItems" :key="item.text" :class="{ selected: $route.path === item.route }">
-                  <router-link class="fv-block" :to="item.route">{{item.text}}</router-link>
-              </fvListItem>
-            </fvList>
-        </fvListItem>
-      </fvList>
-      <fvList v-else>
-        <fvListItem v-for="item in searchResult" :key="item.text" :class="{ selected: $route.path === item.route }">
-            <router-link class="fv-block" :to="item.route">{{item.text}}</router-link>
         </fvListItem>
       </fvList>
     </div>
@@ -73,7 +23,6 @@
 </template>
 
 <script>
-import sidebarItems from '../sidebar-items.js'
 import appLogo from './appLogo.vue'
 
 export default {
@@ -83,7 +32,6 @@ export default {
   data () {
     return {
       query: '',
-      sidebarItems,
       searchResult: undefined
     }
   },
@@ -100,18 +48,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.app-sidebar {
-  padding: 0;
-  min-width: 250px;
-}
-
-// .unclickable /deep/ {
-//   & > .content{
-//     background: rgba(0, 0, 0, 0.14);
-//     padding: 0.8em 0;
-//     font-weight: bold;
-//   }
-// }
-</style>
