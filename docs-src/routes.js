10a08f00b609bb2528068eff8c6c04b176a7369d
nainemom <nainemom@gmail.com>
2020-01-07 18:17:11 +0330

    add component/* route

--- docs-src/routes.js
+++ docs-src/routes.js
@@ -1,5 +1,7 @@
 import Home from './pages/Home.vue';
+import Component from './pages/Component.vue';
 
 export default [
   { path: '/', component: Home },
+  { path: '/components/:component', component: Component },
 ];
