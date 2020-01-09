889490f349630197c08b459ad8c14271449ee031
Amir Momenian <nainemom@gmail.com>
2020-01-09 16:27:59 +0330

    create get-started page

---
+++ docs-src/pages/GetStarted.vue
@@ -0,0 +1,26 @@
+<template>
+<fvLayout>
+  <appHeader title="Get Started" slot="header" />
+  <appSidebar slot="start-sidebar"/>
+  <fvContent slot="content">
+    <div css-max-width="md" css-padding-y="xl" css-padding-x="md"  css-margin-x="auto">
+      <h2 css-text-size="xl"> How to Install </h2>
+    </div>
+  </fvContent>
+  <appFooter slot="footer" />
+</fvLayout>
+</template>
+
+<script>
+import appHeader from '../components/appHeader.vue';
+import appFooter from '../components/appFooter.vue';
+import appSidebar from '../components/appSidebar.vue';
+
+export default {
+  components: {
+    appHeader,
+    appFooter,
+    appSidebar,
+  },
+}
+</script>
