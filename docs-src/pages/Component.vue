a8ff033fb01aa45e6648a76a7e6ee19a31d32bcf
Amir Momenian <nainemom@gmail.com>
2020-01-09 16:28:32 +0330

    load Framevuerk syncly

--- docs-src/pages/Component.vue
+++ docs-src/pages/Component.vue
@@ -1,36 +1,34 @@
 <template>
-  <fvLayout>
-    <appHeader title="Framevuerk" slot="header">
-      <fvButton tag="router-link" to="/installation/setup" css-color="header" css-border="no" css-shadow="no">
-        Documentions
-      </fvButton>
-    </appHeader>
-    <appSidebar slot="start-sidebar"/>
-    <fvContent slot="content">
-      <div css-max-width="md" css-padding-y="xl" css-padding-x="md"  css-margin-x="auto">
-        <h2 css-text-size="xl"> Code </h2>
-        <div css-border="md" css-shadow="md" css-radius="md" css-padding="md" css-color="sidebar">
-          <div v-text="exampleSourceCode" />
-        </div>
-        <div css-margin-y="lg" />
-        <h2 css-text-size="xl"> Example </h2>
-        <div css-border="md" css-shadow="md" css-radius="md" css-padding="md" css-color="background">
-          <component :is="componentExample" />
-        </div>
-        <h2 css-text-size="xl"> API </h2>
-        <div css-border="md" css-shadow="md" css-radius="md" css-padding="md" css-color="background">
-          <div v-text="componentApi" />
-        </div>
-        
+<fvLayout>
+  <appHeader :title="'Framevuerk - ' + api.displayName" slot="header" />
+  <appSidebar slot="start-sidebar"/>
+  <fvContent slot="content">
+    <div css-max-width="md" css-padding-y="xl" css-padding-x="md"  css-margin-x="auto">
+      <h2 css-text-size="xl"> Code </h2>
+      <div css-border="md" css-shadow="md" css-radius="md" css-padding="md" css-color="sidebar">
+        <div v-text="code" />
       </div>
-    </fvContent>
-    <fvFooter slot="footer" css-padding="lg" css-text-align="center" cssz-color="background"> Released under the MIT License Copyright © 2016-{{new Date().getFullYear()}} Amir Momenian. </fvFooter>
-  </fvLayout>
+      <div css-margin-y="lg" />
+      <h2 css-text-size="xl"> Example </h2>
+      <div css-border="md" css-shadow="md" css-radius="md" css-padding="md" css-color="background">
+        <component :is="example" />
+      </div>
+      <h2 css-text-size="xl"> API </h2>
+      <div css-border="md" css-shadow="md" css-radius="md" css-padding="md" css-color="background">
+        <div v-text="api" />
+      </div>
+      
+    </div>
+  </fvContent>
+  <appFooter slot="footer" />
+</fvLayout>
 </template>
 
 <script>
 import appHeader from '../components/appHeader.vue';
+import appFooter from '../components/appFooter.vue';
 import appSidebar from '../components/appSidebar.vue';
+import * as Framevuerk from 'framevuerk';
 
 function textAsTitle(text) {
   return text[0].toUpperCase() + text.substr(1).toLowerCase()
@@ -40,18 +38,14 @@ function textToLower(text) {
 }
 
 function LoadComponent(name, part = null) {
-  return new Promise((resolve) => {
-    import('framevuerk').then((Framevuerk) => {
-      const res = Framevuerk[textAsTitle(name)];
-      resolve(part ? res[part] : res);
-    });
-  })
+  const res = Framevuerk[textAsTitle(name)];
+  return part ? res[part] : res;
 }
 
 function componentsExample(list) {
   const ret = {};
   list.forEach((component) => {
-    ret[textToLower(component) + 'Example'] = () => LoadComponent(component, '__example');
+    ret[textToLower(component) + 'Example'] = Framevuerk[textAsTitle(component)].__example || { template: '<div> No Example </div>' };
   });
   return ret;
 }
@@ -59,28 +53,37 @@ function componentsExample(list) {
 export default {
   components: {
     appHeader,
+    appFooter,
     appSidebar,
     ...componentsExample([
-      'footer',
-      'header',
+      'Footer',
+      'Header',
     ]),
   },
   data() {
     return {
-      exampleSourceCode: '',
-      componentApi: {},
+      code: '',
+      api: {},
     }
   },
   created() {
-    LoadComponent(this.$route.params.component).then((res) => {
-      this.exampleSourceCode = res.__exampleSource;
-      this.componentApi = res.__api;
-    });
+    this.loadData(this.$route.params.component);
+  },
+  methods: {
+    loadData(component) {
+      this.code = Framevuerk[textAsTitle(component)].__exampleSource
+      this.api = Framevuerk[textAsTitle(component)].__api
+    }
   },
   computed: {
-    componentExample() {
+    example() {
       return textToLower(this.$route.params.component) + 'Example';
     },
   },
+  watch: {
+    '$route.params.component'(newComponent) {
+      this.loadData(newComponent);
+    }
+  }
 }
 </script>
