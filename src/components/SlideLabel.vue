caa92eb66c6c131e8f83d3b2177a02094e336644
Amir Momenian <nainemom@gmail.com>
2020-01-13 21:57:55 +0330

    create SlideLabel component

---
+++ src/components/SlideLabel.vue
@@ -0,0 +1,45 @@
+<template>
+<button :class="[$style.slideLabel, isSelected && 'selected']" @click="onClick">
+  <slot />
+</button>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  inject: ['$theme', '$slider'],
+  computed: {
+    isSelected() {
+      return this.$slider && this.$slider.current === this.name;
+    }
+  },
+  methods: {
+    onClick() {
+      this.$slider.setCurrent(this.name);
+    },
+  },
+  style({className}) {
+    return [
+      className('slideLabel', {
+        userSelect: 'none',
+        overflowX: 'hidden',
+        flexGrow: 1,
+        borderBottomWidth: '3px',
+        borderBottomColor: 'transparent',
+        height: this.$theme.sizes.base.factor('md', 'height'),
+        fontWeight: 'bold',
+        transition: 'border-bottom-color 200ms',
+        '&.selected': {
+          color: this.$theme.colors.primary.normal,
+          borderBottomColor: this.$theme.colors.primary.normal,
+        }
+      })
+    ];
+  },
+}
+</script>
