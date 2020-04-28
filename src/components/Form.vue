761b5a8056e6f8801a1f1a2736e65a265267bee3
Amir Momenian <nainemom@gmail.com>
2020-04-28 18:14:25 +0430

    make it works

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -34,55 +34,25 @@
 export default {
   methods: {
     submit() {
-      function isValidate(child) {
-        if (typeof child.disabled !== 'undefined' && child.disabled) {
-          return true;
-        }
-        if (typeof child.isValidate !== 'undefined' && !child.isValidate) {
-          return false;
-        }
-        return true;
-      }
-      function focus(child) {
-        if (typeof child.focus === 'function') {
-          child.focus();
-        } else if (typeof child.$el.focus === 'function') {
-          child.$el.focus();
-        }
-      }
-      function reject(child) {
-        if (typeof child.reject === 'function') {
-          child.reject();
-        }
-      }
-      function getChilds(parent, force = false) {
-        let ret = [];
-        if (
-          (parent.$el.tagName !== 'FORM' || typeof parent.isValidate !== 'undefined')
-          && !force) {
-          return [];
-        }
-        parent.$children.forEach((child) => {
-          ret.push(child);
-          if (child.$children.length) {
-            ret = ret.concat(getChilds(child, false));
-          }
-        });
-        return ret;
-      }
+      const invalids = (() => {
+        const insideFunc = (el) => {
+          let ret = [];
+          el.$children.forEach((child) => {
+            if (child.isValidate === false) {
+              ret.push(child);
+              child.reject();
+            }
+            if (child.$children.length) {
+              ret = ret.concat(insideFunc(child));
+            }
+          });
+          return ret;
+        };
+        return insideFunc(this);
+      })();
 
-      const childs = getChilds(this, true);
-
-      const invalids = [];
-
-      childs.forEach((child) => {
-        if (!isValidate(child)) {
-          invalids.push(child);
-          reject(child);
-        }
-      });
       if (invalids.length) {
-        focus(invalids[0]);
+        invalids[0].focus();
         this.$emit('reject', invalids);
       } else {
         this.$emit('submit');
