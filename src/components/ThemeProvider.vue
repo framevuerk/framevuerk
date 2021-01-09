046ee6ee94ccc7e441677a3105bec4bddee3ee0e
Amir Momenian <nainemom@gmail.com>
2021-01-09 11:22:41 +0330

    upodate sm font-factor

--- src/components/ThemeProvider.vue
+++ src/components/ThemeProvider.vue
@@ -115,7 +115,7 @@ export default {
       const sizeFactor = [0, 0.25, 0.5, 1, 3, 9];
       const shadowFactor = [0, 0.25, 0.5, 1, 1.5, 2.5];
       const shadowBlurFactor = [0, 0.5, 0.75, 1, 1.25, 1.5];
-      const fontFactor = [0, 0.6, 0.8, 1, 1.15, 1.8];
+      const fontFactor = [0, 0.7, 0.8, 1, 1.15, 1.8];
       const borderFactor = [0, 0.0625, 0.125, 0.125, 0.1875, 0.25];
       const factors = ['no', 'xs', 'sm', 'md', 'lg', 'xl', 'round'];
       const ret = {};
